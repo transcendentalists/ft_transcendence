4919011f92f1b7cc59dfcb35127775f0621a4fe3
humblEgo <injun.woo30000@gmail.com>
2021-01-30 20:24:46 +0900

    refactor: Backbone router 설계 수정
    router로 연결되는 URL의 path fragment를 param1, param2 로 맵핑하여
    해당 값들을 검사하여 렌더링할 view를 특정할 수 있도록 수정하였습니다.
    ex) /guilds ==> param1: nil, param2: nil
    ex) /guilds/2 ==> param1: 2, param2: nil
    ex) /guilds/warrequests/new ==> param1: warrequests, param2: new

--- project/app/javascript/packs/router.js
+++ project/app/javascript/packs/router.js
@@ -6,60 +6,64 @@ import { App } from "./internal";
 
 export let Router = Backbone.Router.extend({
   routes: {
-    "users(/*param)": "usersController",
+    "users(/:param1)(/:param2)": "usersController",
     home: "homeController",
-    "chatrooms(/*param)": "chatRoomsController",
-    "guilds(/*param)": "guildsController",
-    "ladder(/*param)": "ladderController",
-    "lives(/*param)": "livesController",
+    "chatrooms(/:param)": "chatRoomsController",
+    "guilds(/:param1)(/:param2)": "guildsController",
+    "ladder(/:page)": "ladderController",
+    "lives(/:matchtype)": "livesController",
     war: "warController",
     "matches/:id": "matchesController",
-    "tournaments(/*param)": "tournamenstController",
-    "admin(/*param)": "adminController",
+    "tournaments(/:param)": "tournamenstController",
+    "admin(/:param)": "adminController",
     "errors/:id": "errorsController",
   },
 
-  usersController(param) {
-    App.entry_view.render(param);
+  // usersController(params) {
+  //   App.entry_view.render(params);
+  // },
+
+  // homeController() {
+  //   App.main_view.renderHome();
+  // },
+
+  // chatRoomsController(params) {
+  //   App.main_view.renderChatRooms(params);
+  // },
+
+  guildsController(param1, param2) {
+    console.log("PARAM 1");
+    console.log(param1);
+    console.log("PARAM 2");
+    console.log(param2);
+    // App.main_view.renderGuilds(params);
   },
 
-  homeController() {
-    App.main_view.renderHome();
-  },
+  // ladderController(params) {
+  //   App.main_view.renderladder(params);
+  // },
 
-  chatRoomsController(param) {
-    App.main_view.renderChatRooms(param);
-  },
+  // livesController(params) {
+  //   App.main_view.renderLives(params);
+  // },
 
-  guildsController(param) {
-    App.main_view.renderGuilds(param);
-  },
+  // warController() {
+  //   App.main_view.renderWar();
+  // },
 
-  ladderController(param) {
-    App.main_view.renderladder(param);
-  },
+  // matchesController(id) {
+  //   App.main_view.renderMatches(id);
+  // },
 
-  livesController(param) {
-    App.main_view.renderLives(param);
-  },
+  // tournamentsController(params) {
+  //   App.main_view.renderTournaments(params);
+  // },
 
-  warController() {
-    App.main_view.renderWar();
-  },
+  // adminController(params) {
+  //   App.main_view.renderAdmin(params);
+  // },
 
-  matchesController(id) {
-    App.main_view.renderMatches(id);
-  },
-
-  tournamentsController(param) {
-    App.main_view.renderTournaments(param);
-  },
-
-  adminController(param) {
-    App.main_view.renderAdmin(param);
-  },
-
-  errorsController(param) {
-    App.main_view.renderError(param);
-  },
+  // errorsController(params) {
+  //   App.main_view.renderError(params);
+  // },
 });

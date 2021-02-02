9ae5fd796302596be69d4dca21640ec02dd9eb4b
Ungchi <dd1360@naver.com>
2021-02-02 23:51:58 +0900

    test: status 가 playing 인 jujeong 수정
    appearance 에서 온라인인 유저 뿐만 아니라 게임 중인 유저도 확인하기
    위해서 status 변경

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -11,7 +11,7 @@ User.create([
   {name: 'yohlee1', password: '123123', email: 'yohan9612@naver.com', image_url: '', point: 2, two_factor_auth: false},
   {name: 'eunhkim1', password: '123123', email: 'valhalla.host@gmail.com', image_url: '', point: 3, two_factor_auth: false},
   {name: 'iwoo1', password: '123123', email: 'injun.woo30000@gmail.com', image_url: '', point: 4, two_factor_auth: true},
-  {name: 'jujeong', password: '123123', email: 'juhyeonjeong92@gmail.com', image_url: '', point: 5, two_factor_auth: false},
+  {name: 'jujeong', password: '123123', email: 'juhyeonjeong92@gmail.com', image_url: '', point: 5, two_factor_auth: false, status: 'playing'},
   {name: 'iwoo2', password: '123123',  email: 'injun.woo30000@gmail.com', image_url: '', point: 7, two_factor_auth: false},
   {name: 'yohlee2', password: '123123',  email: 'yohan9612@yonsei.ac.kr', image_url: '', point: 8},
   {name: 'sanam2', password: '123123',  email: 'simian114@gmail.com', image_url: '', point: 9},

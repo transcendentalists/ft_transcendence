ab772d3c9b56c471e07158b993b9826e12cad74f
humblEgo <injun.woo30000@gmail.com>
2021-02-03 10:22:29 +0900

    feat: server side apperance channel 구현
    server side apperance channel을 구현했습니다.
    쿠키에 저장된 :service_id 를 확인하여 유저를 특정하는 방식이므로,
    쿠키를 이용하는 로그인 방식을 이용할 때만 정상작동합니다.
    prettier가 자동으로 스타일을 바꾼 부분이 있는 점 참고 부탁드립니다.

--- project/app/channels/appearance_channel.rb
+++ project/app/channels/appearance_channel.rb
@@ -1,6 +1,17 @@
 class AppearanceChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    stream_from 'appearance_channel'
+    #  current_user -> 초기화 서버에 나 연결됐어
+    p 'subscribed'
+    p 'subscribed'
+    p 'subscribed'
+    p 'subscribed'
+    p 'current_user: '
+    p current_user
+    return if current_user.nil?
+
+    current_user.notice_login
   end
 
   def unsubscribed
--- project/app/channels/application_cable/connection.rb
+++ project/app/channels/application_cable/connection.rb
@@ -1,4 +1,33 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+    rescue_from StandardError, with: :report_error
+
+    def connect
+      p 'connect'
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      p 'verified_user'
+      p 'verified_user'
+      p 'verified_user'
+      p 'verified_user'
+      p 'verified_user'
+      p 'Debug 1'
+      if User.exists?(cookies.encrypted[:service_id])
+        p 'Debug 2'
+        verified_user = User.find(cookies.encrypted[:service_id])
+      else
+        p 'Debug 4'
+        reject_unauthorized_connection
+      end
+    end
+
+    def report_error(e)
+      p e.message
+    end
   end
 end

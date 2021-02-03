1bd87be8a8ce92128eac05306477433805b9c8f8
humblEgo <injun.woo30000@gmail.com>
2021-02-03 18:18:25 +0900

    style: parameter 전달 방식을 airbnb 스타일로 변경

--- project/app/controllers/application_controller.rb
+++ project/app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-  def create_session id
+  def create_session(id)
     cookies.encrypted[:service_id] = id
   end
 
   def remove_session
     cookies.encrypted[:service_id] = 0
   end
-
 end

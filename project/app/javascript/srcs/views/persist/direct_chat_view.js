6b6b1b43d4ba57b76ca4bc23d0879cef9bdace5b
sanam <simian114@gmail.com>
2021-02-04 22:18:25 +0900

    resolve: 컨플릭트 제거

--- project/app/javascript/srcs/views/persist/direct_chat_view.js
+++ project/app/javascript/srcs/views/persist/direct_chat_view.js
@@ -1,18 +1,43 @@
+import { Helper } from "srcs/internal";
+
 export let DirectChatView = Backbone.View.extend({
   template: _.template($("#direct-chat-view-template").html()),
   className: "ui text container",
+  el: "#direct-chat-view",
+
+  events: {
+    "click .blue.labeled.button": "test1",
+    "click .ui.button": "test2",
+  },
+
+  test2: function () {
+    Helper.input({
+      subject: "인풋모달",
+      description: "인풋테스트 콜백없음",
+    });
+  },
+
+  test1: function () {
+    Helper.input({
+      subject: "인풋모달",
+      description: "인풋테스트 콜백있음",
+      success_callback: function (description) {
+        console.log(description);
+      },
+    });
+  },
 
   initialize: function () {
-    // this.render();
+    this.$el.hide();
   },
 
   render: function () {
-    this.$el.empty();
     this.$el.html(this.template());
-    $("#footer").append(this.$el);
+    this.$el.show();
   },
 
   close: function () {
-    $("#footer").empty();
+    this.$el.empty();
+    this.$el.hide();
   },
 });

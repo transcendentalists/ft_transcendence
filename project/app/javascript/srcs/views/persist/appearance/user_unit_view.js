937f9e9061703cdab094f99349633246c5715042
sanam <simian114@gmail.com>
2021-02-05 23:32:30 +0900

    fix: 유저메뉴 뷰는 하나만 보이도록 수정
    이전에는 유저메뉴 뷰가 띄어진 상태에서 다른 유저의 메뉴뷰를 볼 수
    있었습니다. 이는 사용경험 자체에 나쁜 영향을 준다고 생각해서 이미
    메뉴뷰가 띄어져 있다면 해당 뷰를 지우고 다른 뷰를 띄우도록 수정했습니다.
    
    여기에서 문제의 소지가 될 수 있는게, 뷰 객체 자체를 삭제하지 않고
    제이쿼리의 remove를 삭제하게 되어있어서 돔요소와 이벤트만 삭제되고
    있습니다. 이 부분은 추후 학습을 더 해서 수정하도록 하면 될 것 같습니다.

--- project/app/javascript/srcs/views/persist/appearance/user_unit_view.js
+++ project/app/javascript/srcs/views/persist/appearance/user_unit_view.js
@@ -15,32 +15,32 @@ export let UserUnitView = Backbone.View.extend({
     this.listenTo(this.model, "change:status", this.changeStatus);
   },
 
-  showUserMenu: function() {
-    console.log(this.model.get("name") + " is clicked!");
-
+  showUserMenu: function () {
+    if ($("#user-menu-view").length) {
+      $("#user-menu-view").remove();
+    }
     let position = this.$el.offset();
     this.user_menu_view = new App.View.UserMenuView({ model: this.model });
     $("#root").append(this.user_menu_view.render(position).$el);
   },
 
   render: function () {
-    window.unit = this.$el
     this.$el.html(
       this.template({
         name: this.model.get("name"),
         status: this.model.get("status"),
-      })
+      }),
     );
     return this;
   },
 
-  changeStatus: function() {
+  changeStatus: function () {
     if (this.model.get("status") == "playing") {
-      this.$el.find('.circular').removeClass("green");
-      this.$el.find('.circular').addClass("yellow");
+      this.$el.find(".circular").removeClass("green");
+      this.$el.find(".circular").addClass("yellow");
     } else if (this.model.get("status") == "online") {
-      this.$el.find('.circular').removeClass("yellow");
-      this.$el.find('.circular').addClass("green");
+      this.$el.find(".circular").removeClass("yellow");
+      this.$el.find(".circular").addClass("green");
     }
   },
 

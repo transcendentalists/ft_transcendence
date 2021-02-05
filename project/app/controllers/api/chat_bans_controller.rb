b096fab6c1d8996eb83981fcd9a65cb085f1741d
sanam <simian114@gmail.com>
2021-02-05 22:36:26 +0900

    feat: chatBanParams 함수 및 create 구현
    chatBanParams 는 private 함수로 strong-parameter 로 들어온 param 을
    처리합니다. 여기에서의 이슈는 params로 들어오는 데이터가 두 종류라는
    점입니다. user_id 는 쿼리로 날아오고 밴 당하는 유저는 user 라는 제이슨
    객체로 날아오기 때문에 params.require 로 한번에 처리하지 못했습니다.
    학습 결과로 제이슨 객체와 일반 객체를 permit 하나로 받아올 수 있게
    구현했습니다.
    
    params.permit(:user_id, banned_user: [:id]) 처럼 제이슨 객체로 들어오는
    데이터는 배열로 묶고 심볼로 나타내면 됩니다.

--- project/app/controllers/api/chat_bans_controller.rb
+++ project/app/controllers/api/chat_bans_controller.rb
@@ -1,13 +1,19 @@
 class Api::ChatBansController < ApplicationController
   def index
-    render plain: params[:user_id] + "'s chat ban list"
+    render plain: chatBanParams[:user_id] + "'s chat ban list"
   end
 
   def create
-    render plain: params[:user_id] + " create new chat ban"
+    ChatBan.newChatBan(chatBanParams);
   end
 
   def destroy
     render plain: params[:user_id] + " destroy chat ban " + params[:id]
+  end
+
+  private
+
+  def chatBanParams
+    params.permit(:user_id, banned_user: [:id])
   end
 end

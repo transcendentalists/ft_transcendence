5a2b1267d3c8450044a7e929bfe6e6530434ccf1
sanam <simian114@gmail.com>
2021-02-05 19:03:19 +0900

    feat: 리스트에 있는 유저에 클릭 이벤트 추가
    유저 리스트를 클릭하면 유저 메뉴가 show 되도록 만들었습니다.

--- project/app/javascript/srcs/views/persist/appearance/user_unit_view.js
+++ project/app/javascript/srcs/views/persist/appearance/user_unit_view.js
@@ -1,14 +1,30 @@
+import { Helper } from "../../../helper";
+import { App } from "../../../internal";
+
 export let UserUnitView = Backbone.View.extend({
   tagName: "div",
   className: "item",
   template: _.template($("#appearance-user-list-unit-template").html()),
+
+  events: {
+    "click ": "showUserMenu",
+  },
 
   initialize: function () {
     this.listenTo(this.model, "remove", this.close);
     this.listenTo(this.model, "change:status", this.changeStatus);
   },
 
+  showUserMenu: function() {
+    console.log(this.model.get("name") + " is clicked!");
+
+    let position = this.$el.offset();
+    this.user_menu_view = new App.View.UserMenuView({ model: this.model });
+    $("#root").append(this.user_menu_view.render(position).$el);
+  },
+
   render: function () {
+    window.unit = this.$el
     this.$el.html(
       this.template({
         name: this.model.get("name"),
@@ -19,7 +35,13 @@ export let UserUnitView = Backbone.View.extend({
   },
 
   changeStatus: function() {
-    this.render();
+    if (this.model.get("status") == "playing") {
+      this.$el.find('.circular').removeClass("green");
+      this.$el.find('.circular').addClass("yellow");
+    } else if (this.model.get("status") == "online") {
+      this.$el.find('.circular').removeClass("yellow");
+      this.$el.find('.circular').addClass("green");
+    }
   },
 
   close: function () {

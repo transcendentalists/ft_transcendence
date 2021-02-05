0404c7846e565f7faf16fbf66716a623caf98515
sanam <simian114@gmail.com>
2021-02-05 21:02:04 +0900

    feat: 유저 메뉴 버튼 활성화
    유저메뉴의 템플릿 파일과 뷰 파일을 수정했습니다.
    뷰에서 유저메뉴의 각 DOM 요소의 이벤트을 위임받게했습니다.
    각 이벤트의 CSS 선택자는 data 문법을 이용해서 구현했습니다.
    
    아직 각 버튼마다의 함수는 정의하지 않았습니다.

--- project/app/javascript/srcs/views/persist/appearance/user_menu_view.js
+++ project/app/javascript/srcs/views/persist/appearance/user_menu_view.js
@@ -1,10 +1,41 @@
+import { App } from "../../../internal";
+
 export let UserMenuView = Backbone.View.extend({
   template: _.template($("#user-menu-view-template").html()),
 
   events: {
     mouseleave: "close",
-    // mousedown: "close",
-    // blur: "close",
+    "click [data-event-name=direct-chat]": "directChat",
+    "click [data-event-name=chat-ban]": "chatBan",
+    "click [data-event-name=new-friend]": "newFriend",
+    "click [data-event-name=battle]": "battle",
+    "click [data-event-name=user-ban]": "userBan",
+  },
+
+  directChat: function() {
+    // this.model
+    App.appView.direct_chat_view.render();
+    console.log("directChat!!");
+  },
+
+  chatBan: function() {
+    // this.model
+    console.log("chatBan!!");
+  },
+
+  newFriend: function() {
+    // this.model
+    console.log("newFriend!!");
+  },
+
+  battle: function() {
+    // this.model
+    console.log("battle!!");
+  },
+
+  userBan: function() {
+    // this.model
+    console.log("userBan!!");
   },
 
   render: function (position) {

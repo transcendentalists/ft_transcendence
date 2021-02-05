4cd51ae9fac6f390683e7a0133473e619fa1a39c
sanam <simian114@gmail.com>
2021-02-05 22:34:53 +0900

    feat: newChatBan 함수 구현
    해당 함수는 컨트롤러에 strong - parameter로 처리된 인자를 받아서 존재
    여부를 exists 로 검사하고 없으면 추가하는 함수입니다.

--- project/app/models/chat_ban.rb
+++ project/app/models/chat_ban.rb
@@ -1,4 +1,11 @@
 class ChatBan < ApplicationRecord
   belongs_to :user
   belongs_to :banned_user, class_name: "User", :foreign_key => "banned_user_id"
+
+  def self.newChatBan(chatBanParams)
+    unless exists?(user_id: chatBanParams[:user_id], banned_user_id: chatBanParams[:banned_user][:id])
+      create(user_id: chatBanParams[:user_id], banned_user_id: chatBanParams[:banned_user][:id])
+    end
+  end
+
 end

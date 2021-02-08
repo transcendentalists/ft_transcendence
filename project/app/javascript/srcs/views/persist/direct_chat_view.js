92f7dbfe2dd9d3ddc15a2f046ecf632c5ee99164
eunhyulkim <valhalla.host@gmail.com>
2021-02-08 20:42:24 +0900

    style: 테스트 코드 제거
    css 리팩토링을 진행하는 과정에서 테스트 목적으로 사용했던
    불필요한 코드들을 제거했습니다.

--- project/app/javascript/srcs/views/persist/direct_chat_view.js
+++ project/app/javascript/srcs/views/persist/direct_chat_view.js
@@ -3,34 +3,8 @@ import { CurrentUserChatMessageView } from "../share/current_user_chat_message_v
 
 export let DirectChatView = Backbone.View.extend({
   template: _.template($("#direct-chat-view-template").html()),
-  message_template: _.template($("#chat-message-view-template").html()),
-  current_user_message_template: _.template(
-    $("#current-user-chat-message-view-template").html()
-  ),
   className: "ui text container",
   el: "#direct-chat-view",
-
-  events: {
-    "click .blue.labeled.button": "test1",
-    "click .ui.button": "test2",
-  },
-
-  test2: function () {
-    Helper.input({
-      subject: "인풋모달",
-      description: "인풋테스트 콜백없음",
-    });
-  },
-
-  test1: function () {
-    Helper.input({
-      subject: "인풋모달",
-      description: "인풋테스트 콜백있음",
-      success_callback: function (description) {
-        console.log(description);
-      },
-    });
-  },
 
   initialize: function () {
     this.$el.hide();
@@ -38,13 +12,6 @@ export let DirectChatView = Backbone.View.extend({
 
   render: function () {
     this.$el.html(this.template());
-    for (let i = 0; i < 5; ++i) {
-      this.$(".comments").append(this.message_template());
-    }
-    for (let i = 0; i < 8; ++i) {
-      this.$(".comments").append(this.current_user_message_template());
-    }
-
     this.$el.show();
   },
 

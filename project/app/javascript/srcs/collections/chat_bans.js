f16c95746e75dc1d5e97ebdb562e9e9b8ae8c2f1
humblEgo <injun.woo30000@gmail.com>
2021-02-08 18:40:37 +0900

    feat: chat_bans로 create/deleteChatBan 이동
    Backbone 아키텍쳐에서는 view는 컨트롤러, 화면 렌더링의 책임을 가지고
    모델이 비즈니스 로직을, 컬렉션이 모델의 관리(생성, 삭제, 검색 등)을
    하도록 하는 것을 권장합니다. 결과적으로 유지보수가 쉬운 코드를 만드는데
    도움이 될거라는 판단하에 view에 위치한 모델 생성/삭제 함수들을 컬렉션으로
    이동시켰습니다.

--- project/app/javascript/srcs/collections/chat_bans.js
+++ project/app/javascript/srcs/collections/chat_bans.js
@@ -1,4 +1,4 @@
-import { App } from "../internal";
+import { App, Helper } from "../internal";
 import { ChatBan } from "../models/chat_ban";
 
 export let ChatBans = Backbone.Collection.extend({
@@ -16,5 +16,38 @@ export let ChatBans = Backbone.Collection.extend({
     if (typeof this.findWhere({ banned_user_id: user_id }) == "undefined")
       return false;
     else return true;
+  },
+
+  createChatBan: function (user_id) {
+    Helper.fetch(
+      `users/${App.current_user.id}/chat_bans`,
+      this.createChatBanParams(user_id)
+    );
+  },
+
+  createChatBanParams: function (user_id) {
+    return {
+      method: "POST",
+      success_callback: this.fetch.bind(this),
+      body: {
+        banned_user: {
+          id: user_id,
+        },
+      },
+    };
+  },
+
+  destroyChatBan: function (user_id) {
+    Helper.fetch(
+      `users/${App.current_user.id}/chat_bans/${user_id}`,
+      this.destroyChatBanParams()
+    );
+  },
+
+  destroyChatBanParams: function () {
+    return {
+      method: "DELETE",
+      success_callback: this.fetch.bind(this),
+    };
   },
 });

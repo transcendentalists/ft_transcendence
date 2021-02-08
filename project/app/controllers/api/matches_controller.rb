828d86fc5b0393474a032287e6b87d65e7922025
eunhyulkim <valhalla.host@gmail.com>
2021-02-08 16:41:13 +0900

    style: 래더 매치 기본 룰 클래식으로 설정

--- project/app/controllers/api/matches_controller.rb
+++ project/app/controllers/api/matches_controller.rb
@@ -35,7 +35,7 @@ class Api::MatchesController < ApplicationController
 
   private
   def find_or_create_ladder_match_for(user)
-    match = Match.where(match_type: "ladder", status: "pending").first_or_create(rule_id: 5)
+    match = Match.where(match_type: "ladder", status: "pending").first_or_create(rule_id: 1)
     side = match.users.count == 0 ? "left" : "right"
     card = Scorecard.create(user_id: user.id, match_id: match.id, side: side)
     user.update(status: "playing")

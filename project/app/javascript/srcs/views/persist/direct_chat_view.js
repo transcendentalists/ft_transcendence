0fe3fb0efb37c90fd1a6e74f697e6a436a643b0d
eunhyulkim <valhalla.host@gmail.com>
2021-02-09 13:13:50 +0900

    style: 2차 인증 메일 내용 중 테스트 목적 텍스트 제거

--- project/app/javascript/srcs/views/persist/direct_chat_view.js
+++ project/app/javascript/srcs/views/persist/direct_chat_view.js
@@ -1,5 +1,4 @@
 import { Helper } from "srcs/internal";
-import { CurrentUserChatMessageView } from "../share/current_user_chat_message_view";
 
 export let DirectChatView = Backbone.View.extend({
   template: _.template($("#direct-chat-view-template").html()),

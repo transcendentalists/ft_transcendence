72c943e513bb06e87038d560833b3435c5907cf4
Ungchi <dd1360@naver.com>
2021-02-10 19:03:06 +0900

    refactor: 로그인시 상태를 업데이트하고 바로 방송하도록 변경

--- project/app/channels/appearance_channel.rb
+++ project/app/channels/appearance_channel.rb
@@ -1,13 +1,10 @@
 class AppearanceChannel < ApplicationCable::Channel
   def subscribed
     stream_from 'appearance_channel'
-    current_user.update_status("online")
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
-    current_user.logout
-    current_user.update_status("offline")
     # remove_session
   end
 end

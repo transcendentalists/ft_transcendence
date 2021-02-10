df70313cf54c847bbd9b2ba54ebbcbaa88ed7088
Ungchi <dd1360@naver.com>
2021-02-10 13:27:40 +0900

    fix: chat_ban_controller 동작 방식 수정
    기존 모델에서 처리했던 create, destroy 함수를 컨트롤러에서 수행하도록
    수정

--- project/app/controllers/api/chat_bans_controller.rb
+++ project/app/controllers/api/chat_bans_controller.rb
@@ -5,15 +5,18 @@ class Api::ChatBansController < ApplicationController
   end
 
   def show
-    chat_ban = ChatBan.where(user_id: params[:user_id]).find_by_banned_user_id(params[:banned_user_id])
+    chat_ban = ChatBan.find_by_user_id_and_banned_user_id(params[:user_id], params[:id])
     render json: { chatBan: chat_ban }
   end
 
   def create
-    ChatBan.newChatBan(chatBanParams)
+    # ChatBan.find_or_create_by(user_id: chatBanParams[:user_id], banned_user_id: chatBanParams[:banned_user][:id])
+    # ChatBan.find_or_create_by(Hash(*chatBanParams))
+    ChatBan.find_or_create_by(chatBanParams)
   end
 
   def destroy
+    ChatBan.find(params[:id]).destroy
     # params[:user_id], params[:id]
     ChatBan.destroyChatBan(chatBanParams)
   end
@@ -21,6 +24,6 @@ class Api::ChatBansController < ApplicationController
   private
 
   def chatBanParams
-    params.permit(:user_id, :id, banned_user: [:id])
+    params.permit(:user_id, :banned_user_id)
   end
 end

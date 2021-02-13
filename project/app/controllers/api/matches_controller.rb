c602ade244527141b810e683aa8f79cdb1444e17
Ungchi <dd1360@naver.com>
2021-02-13 22:13:52 +0900

    feat: matches DELETE 추가 및 Dual 게임 생성과 참여
    듀얼 게임에서 거절을 하는 경우 기존에 생성했던 매치를 삭제해야 하는
    경우가 생겨서 DELETE 를 추가하였습니다.
    유저 메뉴에서 대전 신청을 누르면 create dual match 에서 듀얼 매치를
    생성한뒤
    notification_channel 에 상대방이 누구인지 알려주는 방송을 하게 됩니다.
    방송을 들은 유저는 invite_view 가 뜨고 승락을 누르면 해당 매치 번호를
    찾아서 들어가게 됩니다.

--- project/app/controllers/api/matches_controller.rb
+++ project/app/controllers/api/matches_controller.rb
@@ -15,13 +15,27 @@ class Api::MatchesController < ApplicationController
   def create
     if params[:user_id]
       user = User.find(params[:user_id])
-      match = find_or_create_ladder_match_for(user)
-      render :json => { match: { id: match.id, match_type: 'ladder', user: { id: user.id } } }
+      if params[:for] == "ladder"
+        match = find_or_create_ladder_match_for(user)
+      elsif params[:for] == "dual"
+        unless params[:match_id].nil?
+          match = join_dual_match_for(user, params[:match_id])
+        else
+          match = create_dual_match_for(user, params[:rule_id])
+        end
+      end
+      render :json => { match: { id: match.id, match_type: params[:for], user: { id: user.id } } }
     elsif params[:war_id]
       render plain: "war creates " + params[:war_id] + "'s matches"
     else
       render plain: "post /api/matches/create"
     end
+  end
+
+  def destroy
+    match = Match.find(params[:id])
+    ActionCable.server.broadcast('notification_channel', {type: "MatchCancel", user_id: match.users.first.id})
+    match&.destroy
   end
 
   def join
@@ -38,9 +52,25 @@ class Api::MatchesController < ApplicationController
 
   private
   def find_or_create_ladder_match_for(user)
-    match = Match.where(match_type: "ladder", status: "pending").first_or_create(rule_id: 1)
+    match = Match.where(match_type: "Ladder", status: "pending").first_or_create(rule_id: 1)
     side = match.users.count == 0 ? "left" : "right"
     card = Scorecard.create(user_id: user.id, match_id: match.id, side: side)
+    user.update_status("playing")
+    match
+  end
+
+  def create_dual_match_for(user, rule_id)
+    match = Match.create(match_type: "Dual", status: "pending", rule_id: rule_id)
+    card = Scorecard.create(user_id: user.id, match_id: match.id, side: "left")
+    ActionCable.server.broadcast('notification_channel',
+      {type: "MatchCreate", match_id: match.id, enemy_id: params[:enemy_id], profile: user.profile})
+    user.update_status("playing")
+    match
+  end
+
+  def join_dual_match_for(user, match_id)
+    match = Match.find(match_id)
+    card = Scorecard.create(user_id: user.id, match_id: match_id, side: "right")
     user.update_status("playing")
     match
   end

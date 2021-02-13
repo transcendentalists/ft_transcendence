41680b4538f28dcad35fb16d7b72a4eff700b717
Ungchi <dd1360@naver.com>
2021-02-13 22:34:22 +0900

    feat: invite_view 동작 구현
    invite_view 승낙과 거절 동작을 구현하였습니다.
    승낙시 듀얼 게임을 생성하지 않고 참가만 하기 때문에 쿼리문으로 매치
    타입과 매치 아이디만 전송하여 참가할 수 있도록 하였습니다.
    거절시 이미 생성되었던 매치를 삭제하며 view 를 닫습니다.

--- project/app/javascript/srcs/views/persist/invite_view.js
+++ project/app/javascript/srcs/views/persist/invite_view.js
@@ -1,20 +1,39 @@
-import { Helper } from "srcs/internal";
+import { App, Helper } from "srcs/internal";
 
 export let InviteView = Backbone.View.extend({
   template: _.template($("#invite-view-template").html()),
   el: "#invite-view",
 
+  events: {
+    "click [data-invite-value=approve]": "approve",
+    "click [data-invite-value=cancel]": "cancel",
+  },
+
   initialize: function () {
+    this.match_id = null;
     this.$el.hide();
   },
 
-  render: function () {
+  approve: function () {
+    App.router.navigate(`#/matches?match-type=dual&match-id=${this.match_id}`);
+    this.close();
+  },
+
+  cancel: function () {
+    Helper.fetch(`matches/${this.match_id}`, { method: "DELETE" });
+    this.close();
+  },
+
+  render: function (data, match_id) {
+    if (this.match_id) this.cancel();
+    this.match_id = match_id;
     this.$el.empty();
-    this.$el.html(this.template());
+    this.$el.html(this.template(data));
     this.$el.show();
   },
 
   close: function () {
+    this.match_id = null;
     this.$el.hide();
   },
 });

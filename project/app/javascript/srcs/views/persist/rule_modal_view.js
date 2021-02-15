20482caedc33c24e0e15db55e1ae07fca64066fb
sanam <simian114@gmail.com>
2021-02-16 01:52:44 +0900

    fix: 대전신청 rule 작성 중에 상대방이 나갔을 때
    이전에는 rule 작성중에 상대방이 나가고나서 submit을 하면
    로그아웃되어있는 상대한테 대전신청이 날라갔습니다. 따라서 submit을 하면
    먼저 appearanve_view에 해당 상대가 있는지를 DOM 요소를 통해 검사하고
    있으면 정상적으로 보내고 없거나 game을 이미 시작했으면 info 모달창으로
    대전신청이 실패했음을 알려줍니다.

--- project/app/javascript/srcs/views/persist/rule_modal_view.js
+++ project/app/javascript/srcs/views/persist/rule_modal_view.js
@@ -27,19 +27,45 @@ export let RuleModalView = Backbone.View.extend({
     $("#rule-modal-view.tiny.modal").modal("hide");
   },
 
+  isEnemyOnline: function () {
+    let user_unit_view = $(
+      `#appearance-view:contains("${this.model.get("name")}")`,
+    );
+    if (!user_unit_view.length) return false;
+    let user_unit_views = $("#appearance-view")
+      .find("[data-status=online]")
+      .parent();
+    for (let user_unit_view of user_unit_views) {
+      if (user_unit_view.textContent.trim() == this.model.get("name")) {
+        return true;
+      }
+    }
+    return false;
+  },
+
   submit: function () {
+    let description = null;
+    if (this.isEnemyOnline() == false) {
+      description = "상대방이 로그아웃 했습니다.";
+    } else if (App.current_user.checkDualRequestOrInviteViewExist()) {
+      description = "다른 유저와 대전 신청 중에는 대전 신청이 불가능합니다.";
+    }
+    if (description != null) {
+      Helper.info({
+        subject: "게임 신청 불가능",
+        description: description,
+      });
+      return;
+    }
     let rule_id = $("select[name=rules]").val();
     let rule_name = $("select[name=rules] option:checked").text();
     let target_score = this.$el.find('[name="score"]:checked').val();
-    if (App.current_user.checkDualRequestOrInviteViewExist()) {
-      Helper.info({
-        subject: "게임 신청 불가능",
-        description: "다른 유저와 대전 신청 중에는 대전 신청이 불가능합니다.",
-      });
-      this.close();
-      return;
-    }
-    App.notification_channel.dualRequest(this.model.id, rule_id, rule_name, target_score);
+    App.notification_channel.dualRequest(
+      this.model.id,
+      rule_id,
+      rule_name,
+      target_score,
+    );
     App.appView.request_view.render(this.model.attributes);
     this.close();
   },

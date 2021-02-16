f8583ec0d56fb35182ea2a1e3d05bd2a2d684b28
humblEgo <injun.woo30000@gmail.com>
2021-02-16 15:13:21 +0900

    feat: chat membership destroy 액션
    요구사항 중 chatroom membership 삭제를 구현하였습니다.
    요구사항 중 아직 구현되지 않은 항목은 TODO로 남겨뒀습니다.

--- project/app/controllers/api/group_chat_memberships_controller.rb
+++ project/app/controllers/api/group_chat_memberships_controller.rb
@@ -12,6 +12,22 @@ class Api::GroupChatMembershipsController < ApplicationController
   end
 
   def destroy
-    render plain: params[:group_chat_room_id] + " group chat room destroy " + params[:id] + " membership"
+    # TODO: 
+    # 1. 구독하고 있던 채널 끊기
+    # 2. 나간 멤버가 오너라면 다른 멤버를 오너로 만들 것
+    if GroupChatMembership.deleteChatRoomMembershipOfuser(destroy_params)
+      render plain: "success to " + params[:group_chat_room_id] + 
+                    " group chat room destroy " + params[:id] + " membership"
+    else
+      render_error("NOT FOUND", "해당하는 챗룸/유저 정보가 없습니다.", "404")
+    end
+  end
+
+  private
+
+  def destroy_params
+    params.require(:group_chat_room_id)
+    params.require(:id)
+    params.permit(:group_chat_room_id, :id)
   end
 end

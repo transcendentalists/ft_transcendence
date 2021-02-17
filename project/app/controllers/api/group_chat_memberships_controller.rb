111660a3c3429028be7c608b87c54dd08a2bc67d
eunhyulkim <valhalla.host@gmail.com>
2021-02-18 02:04:16 +0900

    feat: 그룹챗 멤버십 mute 및 position 업데이트 API 구현
    멤버십 속성을 변경하려는 유저와 변경될 유저의 포지션에 따라
    유효성 검사를 진행하여 속성을 변경하도록 하였습니다.

--- project/app/controllers/api/group_chat_memberships_controller.rb
+++ project/app/controllers/api/group_chat_memberships_controller.rb
@@ -8,7 +8,18 @@ class Api::GroupChatMembershipsController < ApplicationController
   end
 
   def update
-    render plain: params[:group_chat_room_id] + " group chat room updates " + params[:id] + " membership"
+    params = update_params
+    render_error("BAD REQUEST", "권한이 없거나 형식이 잘못된 요청입니다.", 400) if params.nil?
+    
+    membership = params[:member_membership]
+    membership = membership.update_mute(params) unless params[:mute].nil?
+    membership = membership&.update_position(params) unless params[:position].nil?
+
+    if membership
+      render :json => { group_chat_membership: membership }
+    else
+      render_error("BAD REQUEST", "권한이 없거나 형식이 잘못된 요청입니다.", 400)
+    end
   end
 
   def destroy
@@ -25,9 +36,18 @@ class Api::GroupChatMembershipsController < ApplicationController
 
   private
 
+  def update_params
+    admin_membership = GroupChatMembership.find_by_user_id_and_group_chat_room_id(params[:admin_id], params[:group_chat_room_id])
+    member_membership = GroupChatMembership.find_by_user_id_and_group_chat_room_id(params[:member_id], params[:group_chat_room_id])
+    return nil if admin_membership.nil? || member_membership.nil?
+    return nil if params[:mute].nil? && params[:position].nil?
+    { admin_membership: admin_membership, member_membership: member_membership, mute: params[:mute], position: params[:position]}
+  end
+
   def destroy_params
     params.require(:group_chat_room_id)
     params.require(:id)
     params.permit(:group_chat_room_id, :id)
   end
+  
 end
--- project/app/models/group_chat_membership.rb
+++ project/app/models/group_chat_membership.rb
@@ -8,4 +8,47 @@ class GroupChatMembership < ApplicationRecord
       params[:id]
     ).destroy
   end
+  
+  def self.updateChatRoomMembership(params)
+    return nil if params.nil?
+    member_membership = params[:member_membership]
+    if params[:mute] && member_membership.position != "owner"
+      member_membership.mute = params[:mute]
+    end
+    return nil if member_membership.save == false
+    member_membership.update_position if params[:position]
+    member_membership
+  end
+
+  def update_mute(params)
+    return nil if params[:mute].nil? || self.position == "owner"
+    self.mute = params[:mute]
+    return nil if save == false
+    ActionCable.server.broadcast(
+      "group_chat_channel_#{self.group_chat_room_id.to_s}",
+      {
+        type: "mute",
+        user_id: self.user_id,
+        mute: self.mute
+      }
+    )
+    self
+  end
+
+  def update_position(params)
+    return nil if params[:position].nil?
+    return nil if params[:admin_membership].position != "owner"
+    self.position = params[:position]
+    return nil if save == false
+    ActionCable.server.broadcast(
+      "group_chat_channel_#{self.group_chat_room_id.to_s}",
+      {
+        type: "position",
+        user_id: self.user_id,
+        position: self.position
+      }
+    )
+    self
+  end
+
 end

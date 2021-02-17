2ac50151f4f9d1bb90baf1bc84bcd253bbdb8a71
humblEgo <injun.woo30000@gmail.com>
2021-02-17 23:58:49 +0900

    feat: destroy group chat membership 구현
    권한있는 유저(본인, 챗룸 오너, 챗룸 관리자, 웹 관리자)의 요청인지 검증한
    후 멤버십을 삭제하도록 구현하였습니다. 단, 웹 관리자의 경우 아직
    어떻게 관리할지 정해지지 않았으므로 TODO로 남겨뒀습니다.
    오너의 멤버십이 삭제될 경우 다른 유저에게 랜덤하게 오너 포지션을 위임해야
    하는데, 이는 chat_room_user_menu 모듈 부 구현과 겹친다고 판단하여 아직
    구현하지 않았고, 추후 구현될 예정입니다.

--- project/app/controllers/api/group_chat_memberships_controller.rb
+++ project/app/controllers/api/group_chat_memberships_controller.rb
@@ -1,4 +1,6 @@
 class Api::GroupChatMembershipsController < ApplicationController
+  before_action :check_headers_and_find_current_user, only: [ :destroy ]
+
   def index
     render plain: params[:group_chat_room_id] + " memberships index"
   end
@@ -12,15 +14,18 @@ class Api::GroupChatMembershipsController < ApplicationController
   end
 
   def destroy
-    # TODO: 
-    # 1. 구독하고 있던 채널 끊기
-    # 2. 나간 멤버가 오너라면 다른 멤버를 오너로 만들 것
-    if GroupChatMembership.deleteChatRoomMembershipOfuser(destroy_params)
-      render plain: "success to " + params[:group_chat_room_id] + 
-                    " group chat room destroy " + params[:id] + " membership"
-    else
-      render_error("NOT FOUND", "해당하는 챗룸/유저 정보가 없습니다.", "404")
-    end
+    # TODO: 나간 멤버가 오너라면 다른 멤버를 오너로 만들 것
+    params = destroy_params
+
+    membership = GroupChatMembership.find_by_group_chat_room_id_and_user_id(params[:group_chat_room_id], params[:id])
+    return render_error("NOT FOUND", "해당하는 챗룸/유저 정보가 없습니다.", "404") if membership.nil?
+
+    return render_error("NOT AUTHORIZED", "권한이 없는 유저입니다.", "403") unless membership.is_authorized_user_to_destroy(@current_user)
+
+    membership.destroy
+
+    return render_error("FAILED TO DESTROY", "멤버십 삭제를 실패했습니다.", "500") unless membership.destroyed?
+    render plain: "success to destroy chat room membership"
   end
 
   private
@@ -30,4 +35,15 @@ class Api::GroupChatMembershipsController < ApplicationController
     params.require(:id)
     params.permit(:group_chat_room_id, :id)
   end
+
+  def check_headers_and_find_current_user
+    if !request.headers['HTTP_CURRENT_USER']
+      return render_error("NOT VALID HEADERS", "필요한 요청 Header가 없습니다.", "400")
+    end
+    @current_user = User.find_by_id(request.headers['HTTP_CURRENT_USER'])
+    if @current_user.nil?
+      return render_error("NOT VALID HEADERS", "요청 Header의 값이 유효하지 않습니다.", "400")
+    end
+  end
+
 end

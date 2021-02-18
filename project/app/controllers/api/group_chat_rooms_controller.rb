c496ef9024fc8dcee3a6659a75e201d28fc6a9d2
humblEgo <injun.woo30000@gmail.com>
2021-02-18 11:26:13 +0900

    fix: 챗룸 member가 아닌 user의 챗룸 show 요청실패
    기존에는 챗룸 멤버가 아닌 유저가 챗룸 show 액션을 요청하고, 비밀번호 등
    입장 요건을 다 충족하였을 때 멤버십을 추가해주는 동작이 빠져있었습니다.
    이젠 챗룸 멤버가 챗룸에 포함되어있지 않다면 멤버십을 생성하며 룸에
    입장합니다.

--- project/app/controllers/api/group_chat_rooms_controller.rb
+++ project/app/controllers/api/group_chat_rooms_controller.rb
@@ -40,6 +40,14 @@ class Api::GroupChatRoomsController < ApplicationController
       return render_error("INVALID PASSWORD", "Password가 일치하지 않습니다.", "403") unless is_valid_password?(group_chat_room)
     end
 
+    # TODO: 웹 관리자일 때의 처리 추가 필요
+    if group_chat_room.memberships.find_by_user_id(@current_user.id).nil?
+      membership = group_chat_room.join(@current_user)
+      if membership.nil?
+        return render_error("OVERSTAFFED", "입장 가능인원을 초과했습니다." "403")
+      end
+    end
+
     render json: { 
       group_chat_room: group_chat_room.for_chat_room_format,
       owner: group_chat_room.owner.for_chat_room_format,

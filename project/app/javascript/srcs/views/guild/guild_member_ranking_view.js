3099d2c8b26aa13c5ef6cac5f9ea7a516c7305e9
l-yohai <yohan9612@naver.com>
2021-02-18 20:40:10 +0900

    FEAT: guild_member_ranking_view 구현
    길드에 속해있는 멤버들의 정보를 각각의 멤버들이 가지고 있는
    포인트 순으로 정렬하여 렌더링하고
    페이지네이션을 적용하여 한 페이지당 10명의 멤버들이 나타나도록 작업하였습니다.

---
+++ project/app/javascript/srcs/views/guild/guild_member_ranking_view.js
@@ -0,0 +1,31 @@
+import { App } from "srcs/internal";
+
+export let GuildMemberRankingView = Backbone.View.extend({
+  template: _.template($("#guild-member-ranking-view-template").html()),
+  id: "guild-member-ranking-view",
+  className: "ui text container",
+
+  initialize: function () {
+    this.child_views = [];
+  },
+
+  addOne: function (member) {
+    let child_view = new App.View.UserProfileCardView();
+    this.child_views.push(child_view);
+    this.$("#guild-member-profile-card-list").append(
+      child_view.render(member).$el
+    );
+  },
+
+  render: function (members) {
+    this.$el.html(this.template());
+    members.forEach(this.addOne, this);
+    return this;
+  },
+
+  close: function () {
+    this.child_views.forEach((child_view) => child_view.close());
+    this.child_views = [];
+    this.remove();
+  },
+});

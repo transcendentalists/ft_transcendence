8b36a7b3b61c1c921f62b26ce685c42f1e2ca1e2
sanam <simian114@gmail.com>
2021-02-20 00:47:32 +0900

    feat: validation 추가 및 for_guild_index 수정
    validation 간단한 몇가지 추가했습니다.
    클래스 메서드로 for_guild_index 를 수정했습니다. 이제 워리퀘스트 모달 뷰
    에서는 이 데이터를 받아서 렌더링ㄷ합니다.

--- project/app/models/war_request.rb
+++ project/app/models/war_request.rb
@@ -3,17 +3,24 @@ class WarRequest < ApplicationRecord
   has_one :war, dependent: :destroy
   has_many :war_statuses, dependent: :destroy
   validates :status, inclusion: { in: ["pending", "approved", "canceled"] }
+  validates :rule_id, inclusion: { in: 1..7 }
+  validates :target_match_score, inclusion: { in: [3, 5, 7, 10] }
+  # validates_datetime :end_date, after: :start_date
+  # validates_datetime :start_date, after: DateTime.now()
 
   scope :for_guild_index, -> (guild_id) do
     WarRequest.joins(:war_statuses).where(war_statuses: {guild_id: guild_id, position: "enemy"}, status: "pending").order(start_date: :asc).map { |request| 
-      challenger_guild_stat = request.war_statuses.find_by_position("challenger")&.guild.guild_stat
-      war_request = request.to_simple
+      challenger_guild_stat = request.war_statuses.find_by_position("challenger")&.guild.profile
+
+      # war_request.start_date = request.start_date.strftime("%F")
+      # war_request.end_date = request.end_date .strftime("%F")
+      # war_request.war_time = request.war_time.strftime("%H")
+      
+      war_request = request.as_json(except: [:start_date, :end_date, :war_time])
+      war_request['start_date'] = request.start_date.strftime("%F")
+      war_request['end_date'] = request.end_date.strftime("%F")
+      war_request['war_time'] = request.war_time.strftime("%H")
       war_request.merge!( challenger: challenger_guild_stat )
     }
-  end
-
-  def to_simple
-    permitted = ["id", "bet_point", "war_time"]
-    data = self.attributes.filter { |field, value| permitted.include?(field) }
   end
 end

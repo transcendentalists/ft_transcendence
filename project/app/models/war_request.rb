665a50fded47bddac9b692762caa3a425d697bf1
sanam <simian114@gmail.com>
2021-02-19 20:31:41 +0900

    refactor: for_guild_index 클래스 메서드 리팩토링

--- project/app/models/war_request.rb
+++ project/app/models/war_request.rb
@@ -2,13 +2,13 @@ class WarRequest < ApplicationRecord
   belongs_to :rule
   has_one :war, dependent: :destroy
   has_many :war_statuses, dependent: :destroy
+  validates :status, inclusion: { in: ["pending", "approved", "canceled"] }
 
   scope :for_guild_index, -> (guild_id) do
-    guild = Guild.find_by_id(guild_id)
-    guild.war_statuses.where(guild_id: guild_id, position: "enemy").map { |war_status|
-      challenger_guild_id = WarStatus.find_by_war_request_id_and_position(war_status.war_request_id, "challenger").guild_id
-      war_request = war_status.request.to_simple
-      war_request.merge!( challenger: Guild.find_by_id(challenger_guild_id).guild_stat )
+    WarRequest.joins(:war_statuses).where(war_statuses: {guild_id: guild_id, position: "enemy"}, status: "pending").order(start_date: :asc).map { |request| 
+      challenger_guild_stat = request.war_statuses.find_by_position("challenger")&.guild.guild_stat
+      war_request = request.to_simple
+      war_request.merge!( challenger: challenger_guild_stat )
     }
   end
 

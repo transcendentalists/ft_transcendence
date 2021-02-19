a2009f75bd2d2506072b76e91340b1f90e6ca752
humblEgo <injun.woo30000@gmail.com>
2021-02-19 15:09:15 +0900

    fix: can_be_destroyed_by? 함수내 잘못된 객체참조
    current_user는 User 모델 레코드이므로 챗룸포지션 정보를 가지고 있지
    않습니다. 이에 연관 membership 정보를 가져와서 포지션을 확인할 수 있도록
    수정하였습니다.

--- project/app/models/group_chat_membership.rb
+++ project/app/models/group_chat_membership.rb
@@ -2,8 +2,22 @@ class GroupChatMembership < ApplicationRecord
   belongs_to :user
   belongs_to :room, class_name: "GroupChatRoom", foreign_key: "group_chat_room_id"
 
+  def requested_by_myself?(current_user)
+    self.user_id == current_user.id
+  end
+
   def can_be_destroyed_by?(current_user)
-    self.user_id == current_user.id || can_be_kicked_by?(current_user.position)
+    return true if requested_by_myself?(current_user)
+    return true if can_be_kicked_by?(current_user)
+
+    false
+  end
+
+  def can_be_kicked_by?(current_user)
+    position_grade = ApplicationRecord.position_grade
+    current_user_position = self.room.memberships.find_by_user_id(current_user.id)&.position
+    return false if current_user_position.nil?
+    position_grade[self.position] <= 2 && position_grade[current_user_position] >= 2
   end
 
   def can_be_muted_by?(current_user_position)
@@ -14,11 +28,6 @@ class GroupChatMembership < ApplicationRecord
   def can_be_position_changed_by?(current_user_position)
     position_grade = ApplicationRecord.position_grade
     position_grade[self.position] <= 2 && position_grade[current_user_position] >= 3
-  end
-
-  def can_be_kicked_by?(current_user_position)
-    position_grade = ApplicationRecord.position_grade
-    position_grade[self.position] <= 2 && position_grade[current_user_position] >= 2
   end
       
   def ghost?

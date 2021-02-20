9c4a0272f0cd53e509d068e269a9521da258366d
sanam <simian114@gmail.com>
2021-02-20 23:25:46 +0900

    feat: guild_memberships에 current_user header 적용
    길드 멤버쉽에서 본인인증 헤더가 적용되는 곳은 두곳입니다.
    1. 가입하는 경우
      - 가입은 본인만 할 수있으므로 current_user 가 본인이 아니면 바로 error
        를 리턴합니다.
    2. 탈퇴하는 경우
      - 본인이 직접
        - 본인 인증 확인 후 탈퇴합니다.
      - 간부에 의해
        - 현재 길드의 길드원이면서 간부인지를 체크합니다

--- project/app/controllers/api/guild_memberships_controller.rb
+++ project/app/controllers/api/guild_memberships_controller.rb
@@ -1,9 +1,15 @@
 class Api::GuildMembershipsController < ApplicationController
+  before_action :check_headers_and_find_current_user, only: [ :create, :update, :destroy ]
+
   def index
     render plain: params[:guild_id] + ' guild membership index'
   end
 
   def create
+    if @current_user.id != params[:user][:id]
+      return render_error("권한 에러", "접근 권한이 없습니다.", 401)
+    end
+
     if not Guild.exists?(params[:guild_id])
       return(
         render_error('길드 가입 실패', '해당 길드 또는 유저가 존재하지 않습니다.', 404)
@@ -32,6 +38,28 @@ class Api::GuildMembershipsController < ApplicationController
   end
 
   def destroy
-    GuildMembership.find_by_id(params[:id])&.destroy
+    membership = GuildMembership.find_by_id(params[:id])
+    if membership.nil?
+      return render_error("길드 탈퇴 실패", "해당 유저의 길드 정보가 존재하지 않습니다.", 401)
+    end
+    if @current_user.guild_membership.guild_id != membership.guild_id || (
+      @current_user.id != membership.user_id &&
+      @current_user.guild_membership.position == "member"
+    )
+      return render_error("권한 에러", "접근 권한이 없습니다.", 401)
+    end
+    membership.destroy
+    head :no_content, status: 204
+  end
+
+  private
+  def check_headers_and_find_current_user
+    if !request.headers['HTTP_CURRENT_USER']
+      return render_error("NOT VALID HEADERS", "필요한 요청 Header가 없습니다.", 400)
+    end
+    @current_user = User.find_by_id(request.headers['HTTP_CURRENT_USER'])
+    if @current_user.nil?
+      return render_error("NOT VALID HEADERS", "요청 Header의 값이 유효하지 않습니다.", 400)
+    end
   end
 end

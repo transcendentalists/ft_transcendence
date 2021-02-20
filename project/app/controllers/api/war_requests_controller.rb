488a3e5785254be15e84592604889dc1ee99d66c
sanam <simian114@gmail.com>
2021-02-20 23:29:00 +0900

    feat: war_requests_controller에 본인인증 적용
    create 는 다른 브랜치의 영역이기에 건들지 않고 update 에만 적용했습니다.
    본인이 해당 길드의 간부인지를 검사합니다.

--- project/app/controllers/api/war_requests_controller.rb
+++ project/app/controllers/api/war_requests_controller.rb
@@ -1,4 +1,6 @@
 class Api::WarRequestsController < ApplicationController
+  before_action :check_headers_and_find_current_user, only: [ :update ]
+
   def index
     if params[:for] == "guild_index"
       render json: { war_requests: WarRequest.for_guild_index(params[:guild_id]) }
@@ -12,11 +14,28 @@ class Api::WarRequestsController < ApplicationController
   end
 
   def update
+    if @current_user.in_guild.nil? ||
+      @current_user.in_guild.id != params[:guild_id].to_i ||
+      @current_user.guild_membership.position == "member" 
+      return render_error("권한 에러", "접근 권한이 없습니다.", 401)
+    end
     war_request = WarRequest.find_by_id(params[:id])
     if war_request.nil?
-      render_error("전쟁 요청 검색 에러", "요청하신 전쟁요청이 존재하지 않습니다.", 404)
+      render_error("전쟁 요청 검색 에러", "요청하신 전쟁 요청이 존재하지 않습니다.", 404)
     end
     war_request.update(status: params[:status]) if params[:status]
     head :no_content, status: 204
   end
+
+  private
+  def check_headers_and_find_current_user
+    if !request.headers['HTTP_CURRENT_USER']
+      return render_error("NOT VALID HEADERS", "필요한 요청 Header가 없습니다.", 400)
+    end
+    @current_user = User.find_by_id(request.headers['HTTP_CURRENT_USER'])
+    if @current_user.nil?
+      return render_error("NOT VALID HEADERS", "요청 Header의 값이 유효하지 않습니다.", 400)
+    end
+  end
+
 end

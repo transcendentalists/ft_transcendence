82ae45105822d1be20118456ec4f34f8a7c66c72
sanam <simian114@gmail.com>
2021-02-24 21:58:37 +0900

    fix: update의 조건 수정
    can_be_updated_by 가 false 가 나오면 에러가 나와야 하는 상황인데,
    true 가 나올 때 에러를 render 해줬었습니다. 따라서 if 에서 unless로
    수정했습니다.

--- project/app/controllers/api/guild_memberships_controller.rb
+++ project/app/controllers/api/guild_memberships_controller.rb
@@ -19,16 +19,15 @@ class Api::GuildMembershipsController < ApplicationController
   end
 
   def update
-    # 2. 버튼을 누르면 페이지 전체가 리로딩 -> 버튼만 바뀌게 수정해야함
-    guild_membership = GuildMembership.find_by_id(params[:id], guild_id: params[:guild_id])
+    guild_membership = GuildMembership.find_by_id_and_guild_id(params[:id], params[:guild_id])
     return render_error('변경 실패', '존재하지 않는 멤버입니다.', 404) if guild_membership.nil?
-    return render_error('권한 에러', '변경 권한이 없습니다.', 401) if guild_membership.can_be_updated_by?(@current_user)
+    return render_error('권한 에러', '변경 권한이 없습니다.', 401) unless guild_membership.can_be_updated_by?(@current_user)
     guild_membership.update(position: params[:position])
     render json: { guildMembership: guild_membership.profile }
   end
 
   def destroy
-    membership = GuildMembership.find_by_id(params[:id], guild_id: params[:guild_id])
+    membership = GuildMembership.find_by_id_and_guild_id(params[:id], params[:guild_id])
     return render_error("탈퇴 실패", "해당 유저의 길드 정보가 존재하지 않습니다.", 404) if membership.nil?
     return render_error("권한 에러", "접근 권한이 없습니다.", 401) unless membership.can_be_destroyed_by?(@current_user)
     membership.destroy

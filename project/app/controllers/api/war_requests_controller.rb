3f438080ead3272abc4785e7c619950f93dae3ab
sanam <simian114@gmail.com>
2021-02-24 12:37:37 +0900

    style: return ... if ... 로 수정

--- project/app/controllers/api/war_requests_controller.rb
+++ project/app/controllers/api/war_requests_controller.rb
@@ -16,35 +16,20 @@ class Api::WarRequestsController < ApplicationController
 
   def update
     war_request = WarRequest.find_by_id(params[:id])
-    if war_request.nil?
-      return render_error("전쟁 요청 검색 에러", "요청하신 전쟁 요청이 존재하지 않습니다.", 404)
-    end
-    return render_error("권한 에러", "접근 권한이 없습니다.", 401) if !war_request.can_be_updated_by(@current_user)
+    return render_error("전쟁 요청 검색 에러", "요청하신 전쟁 요청이 존재하지 않습니다.", 404) if war_request.nil?
+    return render_error("권한 에러", "접근 권한이 없습니다.", 401) unless war_request.can_be_updated_by(@current_user)
 
     if params[:status] == "progress"
-      challenger_guild_id = war_request.war_statuses.find_by_position("challenger")&.guild.id
-      enemy_guild_id = war_request.war_statuses.find_by_position("enemy")&.guild.id
-      if !Guild.find_by_id(enemy_guild_id).requests.find_by_status("progress").nil?
+      if !Guild.find_by_id(war_request.enemy.id).requests.find_by_status("progress").nil?
         return render_error("전쟁 수락 에러", "이미 진행중인 전쟁이 있습니다.", 404)
-      elsif !Guild.find_by_id(challenger_guild_id).requests.find_by_status("progress").nil?
+      elsif !Guild.find_by_id(war_request.challenger.id).requests.find_by_status("progress").nil?
         return render_error("전쟁 수락 에러", "상대 길드가 전쟁을 진행 중입니다.", 404)
       end
+
     end
 
     war_request.update(status: params[:status]) if params[:status]
     War.create(war_request_id: war_request.id, status: "progress")
     head :no_content, status: 204
   end
-
-  private
-  def check_headers_and_find_current_user
-    if !request.headers['HTTP_CURRENT_USER']
-      return render_error("NOT VALID HEADERS", "필요한 요청 Header가 없습니다.", 400)
-    end
-    @current_user = User.find_by_id(request.headers['HTTP_CURRENT_USER'])
-    if @current_user.nil?
-      return render_error("NOT VALID HEADERS", "요청 Header의 값이 유효하지 않습니다.", 400)
-    end
-  end
-
 end

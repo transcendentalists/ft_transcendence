024a6842c9a417b5dbf4124d41f69bd9c61b86f8
sanam <simian114@gmail.com>
2021-02-24 20:53:56 +0900

    chore: 사용하지 않는 파일내용 제거

--- project/app/javascript/srcs/views/guild/guild_create_view.js
+++ project/app/javascript/srcs/views/guild/guild_create_view.js
@@ -1,53 +1,10 @@
 import { App, Helper } from "srcs/internal";
 
 export let GuildCreateView = Backbone.View.extend({
-  template: _.template($("#guild-create-view-template").html()),
-  id: "guild-create-view",
-  className: "create-view top-margin",
-  events: {
-    // "click .create.button": "submit",
-    "click .cancel.button": "cancel",
-  },
+  // template: _.template($("#guild-create-view-template").html()),
 
-  // redirectChatRoomCallback: function (data) {
-  //   App.router.navigate(`#/chatrooms/${data.group_chat_room.id}`);
-  // },
-
-  // createRoomParams: function (input_data) {
-  //   return {
-  //     method: "POST",
-  //     success_callback: this.redirectChatRoomCallback.bind(this),
-  //     fail_callback: (data) => App.router.navigate("#/errors/105"),
-  //     body: {
-  //       group_chat_room: input_data,
-  //     },
-  //   };
-  // },
-
-  // submit: function () {
-  //   let input_data = {};
-  //   input_data["title"] = $("input[name=title]").val();
-  //   input_data["owner_id"] = App.current_user.id;
-  //   input_data["password"] = $("input[name=password]").val();
-  //   input_data["room_type"] = $("input[name=room_type]").is(":checked")
-  //     ? "private"
-  //     : "public";
-  //   input_data["max_member_count"] = +$(
-  //     ".max-member-count option:selected"
-  //   ).val();
-
-  //   Helper.fetch("group_chat_rooms", this.createRoomParams(input_data));
-  // },
-
-  cancel: function () {
-    App.router.navigate("#/guilds");
-  },
 
   render: function () {
-    this.$el.empty();
-    this.$el.html(this.template());
-    this.$(".ui.negative.message").hide();
-    return this;
   },
 
   close: function () {

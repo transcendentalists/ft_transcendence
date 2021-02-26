2ba577fadf8742ebf9bf699cb170e89eba771f89
sanam <simian114@gmail.com>
2021-02-26 17:57:29 +0900

    style: child_views
    guild_member_profile_card_views 를 child_views 로 수정했습니다

--- project/app/javascript/srcs/views/guild/guild_member_list_view.js
+++ project/app/javascript/srcs/views/guild/guild_member_list_view.js
@@ -3,15 +3,14 @@ import { App } from "srcs/internal";
 export let GuildMemberListView = Backbone.View.extend({
   template: _.template($("#guild-member-list-view-template").html()),
 
-  initialize: function (guild_id) {
-    this.child_views = [];
-    this.guild_member_profile_card_views = [];
-    this.guild_id = guild_id;
+  initialize: function (options) {
+    this.child_views = []
+    this.guild_id = options.guild_id;
   },
 
   addOne: function (member) {
     let guild_member_profile_card_view = new App.View.GuildMemberProfileCardView();
-    this.guild_member_profile_card_views.push(guild_member_profile_card_view);
+    this.child_views.push(guild_member_profile_card_view);
     this.$("#guild-member-profile-card-list").append(
       guild_member_profile_card_view.render(member).$el
     );
@@ -24,8 +23,8 @@ export let GuildMemberListView = Backbone.View.extend({
   },
 
   close: function () {
-    this.guild_member_profile_card_views.forEach((guild_member_profile_card_view) => guild_member_profile_card_view.close());
-    this.guild_member_profile_card_views = [];
+    this.child_views.forEach((guild_member_profile_card_view) => guild_member_profile_card_view.close());
+    this.child_views = [];
     this.remove();
   },
 });

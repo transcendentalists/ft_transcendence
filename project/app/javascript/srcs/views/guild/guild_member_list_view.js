d17dc60022df2e7a6b7a5b2e351f30954bb489d2
sanam <simian114@gmail.com>
2021-02-26 17:01:15 +0900

    style: child_view 을 구체적으로 변경
    child_view 를 guild_member_profile_card_view 로 구체적으로 변경했습니다.

--- project/app/javascript/srcs/views/guild/guild_member_list_view.js
+++ project/app/javascript/srcs/views/guild/guild_member_list_view.js
@@ -2,19 +2,18 @@ import { App } from "srcs/internal";
 
 export let GuildMemberListView = Backbone.View.extend({
   template: _.template($("#guild-member-list-view-template").html()),
-  id: "guild-member-list-view",
-  className: "ui text container",
 
   initialize: function (guild_id) {
     this.child_views = [];
+    this.guild_member_profile_card_views = [];
     this.guild_id = guild_id;
   },
 
   addOne: function (member) {
-    let child_view = new App.View.GuildMemberProfileCardView();
-    this.child_views.push(child_view);
+    let guild_member_profile_card_view = new App.View.GuildMemberProfileCardView();
+    this.guild_member_profile_card_views.push(guild_member_profile_card_view);
     this.$("#guild-member-profile-card-list").append(
-      child_view.render(member).$el
+      guild_member_profile_card_view.render(member).$el
     );
   },
 
@@ -25,8 +24,8 @@ export let GuildMemberListView = Backbone.View.extend({
   },
 
   close: function () {
-    this.child_views.forEach((child_view) => child_view.close());
-    this.child_views = [];
+    this.guild_member_profile_card_views.forEach((guild_member_profile_card_view) => guild_member_profile_card_view.close());
+    this.guild_member_profile_card_views = [];
     this.remove();
   },
 });

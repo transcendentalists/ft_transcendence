86d4d633444096a7c1b0a454681abbfc935d4a8f
eunhyulkim <valhalla.host@gmail.com>
2021-02-27 21:08:34 +0900

    feat: tournament 픽스쳐 구현

--- project/spec/factories/tournaments.rb
+++ project/spec/factories/tournaments.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     max_user_count { [8,16,32][Faker::Number.within(range: 0..2)]}
     incentive_title { Faker::Superhero.prefix }
     target_match_score { [3,5,7,10][Faker::Number.within(range: 0..3)]}
-    start_date { Faker::Date.backward(days: 7) }
-    tournament_time { Time.new.change(hour: Faker::Number.within(range: 1..23)) }
+    start_date { Faker::Date.between(from: 2.days.after.midnight, to: 1.week.after.midnight) }
+    tournament_time { Time.zone.now.change(hour: Faker::Number.within(range: 1..23)) }
   end
 end

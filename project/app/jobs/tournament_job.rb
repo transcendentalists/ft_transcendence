725282f5dd547140afe69286a8c2836d9e01d6a7
eunhyulkim <valhalla.host@gmail.com>
2021-02-27 21:02:25 +0900

    feat: 토너먼트 잡 오퍼레이션 구현(테스트 미완료)

--- project/app/jobs/tournament_job.rb
+++ project/app/jobs/tournament_job.rb
@@ -14,63 +14,91 @@ class TournamentJob < ApplicationJob
     end
   end
 
-  def this_round_matches
-    @tournament.matches.where('DATE(start_time) = ?', @now.yesterday.to_date)
-  end
-
-  def this_round_scorecards
-    Scorecard.where(match_id: self.this_round_matches)
-  end
-
-  def update_membership_result
-    if @today_round == 4
-      @tournament.update_progress_memberships_result(result: "bronze")
-    else
-      match = self.this_round_matches.first
-      if match.status == "completed"
-        @tournament.memberships.find(user_id: match.winner.id).update(result: "gold")
-        @tournament.memberships.find(user_id: match.loser.id).update(result: "silver")
-      else
-        @tournament.update_progress_memberships_result(result: "silver")
-      end
-    end
-  end
-
-  def update_membership_status
-    if @today_round == 2
-      @tournament.update_progress_memberships_status("completed")
-    else
-      match_ids = self.this_round_matches.where.not(status: "completed").ids
-      user_ids = self.this_round_scorecards.where(match_id: match_ids).pluck("user_id")
-      @tournament.memberships.where(user_id: user_ids).update_all(status: "completed")
-    end
-  end
-
-  # match & scorecards column update 필요
-  def operate_this_round_matches
-    self.update_mebership_result if @today_round <= 4
-    self.update_membership_status
-    self.this_round_matches.where(status: ["progress", "pending"]).each do |match|
-      match.update(status: "canceled")
-      match.scorecards.update_all(result: "canceled")
-    end
-  end
-
-  def opreate_next_round_matches
-  end
-
   def operate_tournament
-    if @now.to_date == @tournament.start_date.to_date
+    if @tournament.first_date?(@now)
       @tournament.start
       return if @tournament.canceled?
     else
       self.operate_this_round_matches
     end
 
-    if @tournament.memberships.where(status: "progress").exists?
+    if @tournament.progress_memberships.count > 1
       self.opreate_next_round_matches
     else
       @tournament.complete
+    end
+  end
+
+  def operate_this_round_matches
+    self.cancel_exceptional_match
+    self.update_membership_result if @today_round <= 4
+    self.update_membership_status
+  end
+
+  def this_round_matches
+    this_round_day = @now.yesterday.change(hour: @tournament.tournament_time.hour).utc.to_date
+    @tournament.matches.where('DATE(start_time) = ?', this_round_day)
+  end
+
+  def this_round_scorecards
+    Scorecard.where(match_id: self.this_round_matches.ids)
+  end
+
+  def cancel_exceptional_match
+    self.this_round_matches.where(status: ["progress", "pending"]).each do |match|
+      match.update(status: "canceled")
+      match.scorecards.update_all(result: "canceled")
+    end
+  end
+
+  def update_membership_result
+    if @today_round == 4
+      @tournament.update_progress_memberships(result: "bronze")
+    else
+      match = self.last_match
+      if match.completed?
+        @tournament.memberships.find_by_user_id(match.winner.id).update(result: "gold")
+        @tournament.memberships.find_by_user_id(match.loser.id).update(result: "silver")
+      else
+        @tournament.update_progress_memberships(result: "silver")
+      end
+    end
+  end
+
+  def update_membership_status
+    if @today_round == 2
+      @tournament.update_progress_memberships(status: "completed")
+    else
+      user_ids = self.this_round_scorecards.where.not(result: "win").pluck("user_id")
+      @tournament.memberships.where(user_id: user_ids).update_all(status: "completed")
+    end
+  end
+
+  def opreate_next_round_matches
+    membership_ids = @tournament.progress_memberships.ids
+    membership_ids.shuffle!
+    while membership_ids.count > 1 do
+      pair = membership_ids.pop(2)
+      self.create_match(*pair)
+    end
+  end
+
+  def create_match(left_membership_id, right_membership_id)
+    match = Match.create({
+      rule_id: @tournament.rule_id,
+      eventable_type: "Tournament",
+      eventable_id: @tournament.id,
+      match_type: "tournament",
+      start_time: @now.change(hour: @tournament.tournament_time.hour),
+      target_score: @tournament.target_match_score,
+    })
+    card_entry = {left: left_membership_id, right: right_membership_id}
+    card_entry.each do |side, mid|
+      Scorecard.create(
+        user_id: TournamentMembership.find(mid),
+        match_id: match.id,
+        side: side
+      )
     end
   end
 

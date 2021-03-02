0bc641a4db2214187c8208d807c2a71c6df1e69d
humblEgo <injun.woo30000@gmail.com>
2021-03-03 00:22:11 +0900

    style: validate의 start_date 비교방식 수정
    기존 방식에 비해 날짜를 비교하는 의도가 잘 전달되도록 `to_date` 메서드와
    `Date`객체를 활용하도록 수정했습니다.

--- project/app/models/tournament.rb
+++ project/app/models/tournament.rb
@@ -1,6 +1,6 @@
 class TournamentValidator < ActiveModel::Validator
   def validate(record)
-    if record.start_date <= DateTime.current.midnight ||
+    if record.start_date.to_date <= Date.current ||
         invalid_tournament_time?(record.tournament_time)
       record.errors.add :base, "토너먼트 생성은 내일 이후의 일정으로만 생성 가능합니다."
     end

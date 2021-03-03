080686bd87d650d409ff8666f60afd14a38b0d86
humblEgo <injun.woo30000@gmail.com>
2021-03-03 22:18:28 +0900

    fix: Tournament.new() 생성시 valid? 실행가능하도록

--- project/app/validators/tournament_validator.rb
+++ project/app/validators/tournament_validator.rb
@@ -1,7 +1,7 @@
-
 class TournamentValidator < ActiveModel::Validator
   def validate(record)
-    if record.start_date.to_date <= Date.current ||
+    if record.start_date.nil? || 
+        record.start_date.to_date <= Date.current ||
         invalid_tournament_time?(record.tournament_time)
       record.errors.add :base, message: "토너먼트 생성은 내일 이후의 일정으로만 생성 가능합니다."
     end

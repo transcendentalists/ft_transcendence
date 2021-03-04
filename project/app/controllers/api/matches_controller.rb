760e657d6bfe13fcebc7e02909a1ca1d507ee488
eunhyulkim <valhalla.host@gmail.com>
2021-03-04 18:13:19 +0900

    style: 불필요한 코드 및 주석 제거

--- project/app/controllers/api/matches_controller.rb
+++ project/app/controllers/api/matches_controller.rb
@@ -61,9 +61,7 @@ class Api::MatchesController < ApplicationController
   end
 
   def find_or_create_ladder_match_for(user, options = {type: "casual_ladder"} )
-    # 거르는 이유: 중복참여 방지. 게임 중인데 또 게임하는 것 방지하기 위함.
     return nil if user.playing?
-    # return nil unless Scorecard.where(user_id: user.id, result: ["wait", "ready"]).first.nil?
     ActiveRecord::Base.transaction do
       @match = Match.where(match_type: options[:type], status: "pending").last
       @match = Match.create(match_type: options[:type], rule_id: 1) if @match.nil?

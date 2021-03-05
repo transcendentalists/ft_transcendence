1b912341474d325a355985e7b5380d7c8ae850ff
humblEgo <injun.woo30000@gmail.com>
2021-03-05 21:40:33 +0900

    resolve conflict

--- project/db/migrate/20210301131457_add_position_to_users.rb
+++ project/db/migrate/20210301131457_add_position_to_users.rb
@@ -2,8 +2,4 @@ class AddPositionToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :position, :string, default: "user", null: false
   end
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> 62ef8f3cacd915ce1d165bbf93378fd0cdc363d3

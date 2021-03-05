d628563cf0d4da9bbfa980a75248e6f7f702b626
humblEgo <injun.woo30000@gmail.com>
2021-03-05 23:34:13 +0900

    test: can_be_destroyed_by?
    service manager의 권한도 함께 반영되도록 하도록 수정 후 잘 작동하는지
    여부를 확인할 수 있도록 테스트를 작성했습니다.

--- project/spec/models/group_chat_room_spec.rb
+++ project/spec/models/group_chat_room_spec.rb
@@ -21,4 +21,43 @@ RSpec.describe GroupChatRoom, type: :model do
     end
   end
 
+  it "owner, service_manager can_be_kicked_by?" do
+
+    owner = create(:user)
+    chat_room = create_chat_room({owner: owner, register: 3})
+
+    web_owner = create(:user, position: "web_owner")
+    web_admin = create(:user, position: "web_admin")
+    member = create(:user, position: "member")
+    chat_room.join(web_owner)
+    chat_room.join(web_admin)
+    chat_room.join(member)
+
+    memberships = chat_room.memberships
+
+    owner_membership = memberships.find_by_user_id(owner.id)
+    web_owner_membership = memberships.find_by_user_id(web_owner.id)
+    web_admin_membership = memberships.find_by_user_id(web_admin.id)
+    member_membership = memberships.find_by_user_id(member.id)
+
+    expect(owner_membership.can_be_kicked_by?(web_owner)).to eq(true)
+    expect(owner_membership.can_be_kicked_by?(web_admin)).to eq(true)
+    expect(owner_membership.can_be_kicked_by?(member)).to eq(false)
+
+    expect(web_owner_membership.can_be_kicked_by?(web_owner)).to eq(false)
+    expect(web_owner_membership.can_be_kicked_by?(web_admin)).to eq(false)
+    expect(web_owner_membership.can_be_kicked_by?(owner)).to eq(false)
+    expect(web_owner_membership.can_be_kicked_by?(member)).to eq(false)
+
+    expect(web_admin_membership.can_be_kicked_by?(web_owner)).to eq(true)
+    expect(web_admin_membership.can_be_kicked_by?(web_admin)).to eq(false)
+    expect(web_admin_membership.can_be_kicked_by?(owner)).to eq(false)
+    expect(web_admin_membership.can_be_kicked_by?(member)).to eq(false)
+
+    expect(member_membership.can_be_kicked_by?(web_owner)).to eq(true)
+    expect(member_membership.can_be_kicked_by?(web_admin)).to eq(true)
+    expect(member_membership.can_be_kicked_by?(owner)).to eq(true)
+    expect(member_membership.can_be_kicked_by?(member)).to eq(false)
+  end
+
 end

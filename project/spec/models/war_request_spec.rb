42386067c84ea15e5f98ba9ea192bde5eaad5a76
Ungchi <dd1360@naver.com>
2021-03-05 17:42:52 +0900

    test: war_request_spec 테스트 추가

--- project/spec/models/war_request_spec.rb
+++ project/spec/models/war_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe WarRequest, type: :model do
   params = {
     rule_id: 3,
     bet_point: 100,
@@ -15,153 +15,148 @@ RSpec.describe User, type: :model do
     guild_id: 1,
   }
 
-  # begin
-  #   WarRequest.create_by(params)
-  # rescue => e # invalid
-  #   expect(false).to eq(false)
-  # else # valid
-  #   expect(true).to eq(true)
-  # end
-  
-  # 1. user가 member 또는 officer인 상황
-  # 2. 우리 길드가 전쟁 중인 상황
-  # 3. 상대 길드가 전쟁 중인 상황
-  # 4. 이미 상대 길드에 초대장이 가 있는 상황
-  # 5. 
-
-  # it "can be created by hash" do
-  #   begin
-  #     WarRequest.create_by!(params)
-  #   rescue
-  #     expect(false).to eq(true)
-  #   else
-  #     expect(true).to eq(true)
-  #   end
-  # end
-
-  it "no exist challenger guild" do
-    test_id = [1, 2]
-    test_id.each do |i|
-      begin
-      # test_id = [0, 1, "abc", "10", nil]
-        p i
-        params[:guild_id] = i
-        WarRequest.create_by!(params)
-      rescue
-        @ret = false
-        # expect(false).to eq(true)
-      else
-        @ret = true
-        # expect(true).to eq(true)
-      end
-
-      if (@ret)
-        expect(true).to eq(true)
-      else
-        expect(false).to eq(true)
-      end
-
+  Guild.all.each do |guild|
+    it "Cannot create war-request which is in-war" do
+      p = params.clone
+      p[:guild_id] = guild.id
+      expect{ WarRequest.create_by!(p) }.to raise_error if guild.in_war?
     end
   end
 
-  # it "no exist enemy guild" do
-  #   test = [0, 1, "1", "2", nil]
-  #   test.each do |t|
-  #     params[:enemy_guild_id] = t
-  #     begin
-  #       WarRequest.create_by!(params)
-  #     rescue
-  #       expect(false).to eq(true)
-  #     else
-  #       expect(true).to eq(true)
-  #     end
-  #   end
-  # end
-  # it "challenger guild is in war" do
-  #   params[:rule_id] = 100
-  #   begin
-  #     WarRequest.create_by!(params)
-  #   rescue
-  #     expect(false).to eq(true)
-  #   else
-  #     expect(true).to eq(true)
-  #   end
-  # end
-  # it "lack guild point" do
-  #   params[:rule_id] = 100
-  #   begin
-  #     WarRequest.create_by!(params)
-  #   rescue
-  #     expect(false).to eq(true)
-  #   else
-  #     expect(true).to eq(true)
-  #   end
-  # end
+  Guild.all.each do |guild|
+    it "Cannot create war-request which is enemy guild-in-war" do
+      p = params.clone
+      p[:enemy_guild_id] = guild.id
+      expect{ WarRequest.create_by!(p) }.to raise_error if guild.in_war?
+    end
+  end
 
-  
-  # it "cannot be created with invalid rule_id" do
-  #   params[:rule_id] = 100
-  #   begin
-  #     WarRequest.create_by!(params)
-  #   rescue
-  #     expect(false).to eq(true)
-  #   else
-  #     expect(true).to eq(true)
-  #   end
-  # end
+  Guild.all.each do |guild|
+    it "Cannot create war-request with lack guild-point " do
+      p = params.clone
+      p[:guild_id] = guild.id
+      expect{ WarRequest.create_by!(p) }.to raise_error if guild.point < p[:bet_point]
+    end
+  end
 
-  # it "cannot be created with invalid bet_point" do
-  #   params[:bet_point] = "abcd"
-  #   begin
-  #     WarRequest.create_by!(params)
-  #   rescue
-  #     expect(false).to eq(true)
-  #   else
-  #     expect(true).to eq(true)
-  #   end
-  # end
+  [0, nil, "0", "asd", "aa"].each do |i|
+    it "no exist challenger guild" do
+      p = params.clone
+      p[:enemy_guild_id] = i
+      expect{ WarRequest.create_by!(p) }.to raise_error
+    end
+  end
 
-  # it "cannot be created with invalid start_date" do
-  #   params[:start_date] = "2021-03-03"
-  #   begin
-  #     WarRequest.create_by!(params)
-  #   rescue
-  #     expect(false).to eq(true)
-  #   else
-  #     expect(true).to eq(true)
-  #   end
-  # end
+  [2, 4, 7].each do |i|
+    it "exist challenger guild" do
+      p = params.clone
+      p[:enemy_guild_id] = i
+      expect{ WarRequest.create_by!(p) }.not_to raise_error
+    end
+  end
 
-  # it "cannot be created with invalid end_date" do
-  #   params[:war_duration] = 10
-  #   begin
-  #     WarRequest.create_by!(params)
-  #   rescue
-  #     expect(false).to eq(true)
-  #   else
-  #     expect(true).to eq(true)
-  #   end
-  # end
+  [0, nil, "bcd", "xxffqxsaaa32"].each do |i|
+    it "no exist enemy guild" do
+      p = params.clone
+      p[:enemy_guild_id] = i
+      expect{ WarRequest.create_by!(p) }.to raise_error
+    end
+  end
+  [3, 4, 5].each do |i|
+    it "exist enemy guild" do
+      p = params.clone
+      p[:enemy_guild_id] = i
+      expect{ WarRequest.create_by!(p) }.not_to raise_error
+    end
+  end
 
-  # it "cannot be created with invalid max_no_reply_count" do
-  #   params[:max_no_reply_count] = 100
-  #   begin
-  #     WarRequest.create_by!(params)
-  #   rescue
-  #     expect(false).to eq(true)
-  #   else
-  #     expect(true).to eq(true)
-  #   end
-  # end
+  [0, "1", 45, 51, "bbbb", nil].each do |t|
+    it "cannot be created with invalid bet_point" do
+      p = params.clone
+      p[:bet_point] = t
+      expect{ WarRequest.create_by!(p) }.to raise_error
+    end
+  end
+  [100, 150, 500, 950, 1000].each do |t|
+    it "can be created with invalid bet_point" do
+      p = params.clone
+      p[:bet_point] = t
+      expect{ WarRequest.create_by!(p) }.not_to raise_error
+    end
+  end
 
-  # it "cannot be created with invalid target_match_score" do
-  #   params[:target_match_score] = "ab"
-  #   begin
-  #     WarRequest.create_by!(params)
-  #   rescue
-  #     expect(false).to eq(true)
-  #   else
-  #     expect(true).to eq(true)
-  #   end
-  # end
+  (1..7).each do |t|
+    it "can be created with invalid rule_id" do
+      p = params.clone
+      p[:rule_id] = t
+      expect{ WarRequest.create_by!(p) }.not_to raise_error
+    end
+  end
+  [0, "asd", "Test", 1000, nil].each do |t|
+    it "cannot be created with invalid rule_id" do
+      p = params.clone
+      p[:rule_id] = t
+     expect{ WarRequest.create_by!(p) }.to raise_error
+    end
+  end
+
+  (100..1000).step(50).each do |t|
+      p = params.clone
+    it "can be created with invalid bet_point" do
+      p[:bet_point] = t
+      expect{ WarRequest.create_by!(p) }.not_to raise_error
+    end
+  end
+  [1, 10, 40, 51, "test", nil].each do |t|
+    it "Cannot be created with invalid bet_point" do
+      p = params.clone
+      p[:bet_point] = t
+      expect{ WarRequest.create_by!(p) }.to raise_error
+    end
+  end
+
+  ["2021-03-07", "2021-03-08", "2021-05-03"].each do |t|
+    it "Can be created with invalid start_date" do
+      p = params.clone
+      p[:start_date] = t
+      expect{ WarRequest.create_by!(p) }.not_to raise_error
+    end
+  end
+  [0, 20201001, "2021-ads02-04", "202adsa1-03-08", nil].each do |t|
+    it "Cannot be created with invalid start_date" do
+      p = params.clone
+      p[:start_date] = t
+      expect{ WarRequest.create_by!(p) }.to raise_error
+    end
+  end
+
+  (3..10).each do |t|
+    it "can be created with invalid max_no_reply_count" do
+      p = params.clone
+      p[:max_no_reply_count] = t
+      expect{ WarRequest.create_by!(p) }.not_to raise_error
+    end
+  end
+  [0, -1, 'abc', 1000, nil].each do |t|
+    it "cannot be created with invalid max_no_reply_count" do
+      p = params.clone
+      p[:max_no_reply_count] = t
+      expect{ WarRequest.create_by!(p) }.to raise_error
+    end
+  end
+
+  [3, 5, 7, 10].each do |t|
+    it "can be created with invalid target_match_score" do
+      p = params.clone
+      p[:target_match_score] = t
+      expect{ WarRequest.create_by!(p) }.not_to raise_error
+    end
+  end
+  [0, 1, -1, 'abc', nil].each do |t|
+    it "cannot be created with invalid target_match_score" do
+      p = params.clone
+      p[:target_match_score] = t
+      expect{ WarRequest.create_by!(p) }.to raise_error
+    end
+  end
 end

fec3352ca844adc7825726fb652df96a1febb6a4
l-yohai <yohan9612@naver.com>
2021-03-07 17:12:43 +0900

    feat: 전쟁 요청 시 상대 길드포인트 확인하도록 변경

--- project/app/validators/war_status_validator.rb
+++ project/app/validators/war_status_validator.rb
@@ -6,8 +6,11 @@ class WarStatusValidator < ActiveModel::Validator
       error_message = record.position == "enemy" ? "상대 길드가 존재하지 않습니다." : "길드가 있는 유저만 전쟁을 신청 할 수 있습니다." 
     elsif guild.in_war?
       error_message = record.position == "enemy" ? "상대 길드가 진행 중인 전쟁이 있습니다." : "전쟁 중에는 전쟁 신청을 할 수 없습니다."
-    elsif record.position == "challenger" && guild.point < record.request.bet_point
-      error_message = "길드 포인트 부족으로 전쟁 신청을 할 수 없습니다."
+    elsif guild.point < record.request.bet_point
+      error_message =
+        record.position == "challenger" ?
+          "길드 포인트 부족으로 전쟁 신청을 할 수 없습니다." :
+          "상대의 길드 포인트가 부족합니다. (상대 길드 포인트: #{guild.point})"
     end
     record.errors.add(:base, message: error_message) unless error_message.nil?
   end

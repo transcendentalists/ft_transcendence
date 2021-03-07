80e1950fd328c9ace76161f4426a290ceca2cad8
l-yohai <yohan9612@naver.com>
2021-03-07 17:14:50 +0900

    feat: war_request_error 생성자에 default 메세지 추가

--- project/app/errors/war_request_error.rb
+++ project/app/errors/war_request_error.rb
@@ -1,6 +1,6 @@
 class WarRequestError < StandardError
   attr_accessor :message, :status_code
-  def initialize(message, status_code = 400)
+  def initialize(message = "잘못된 요청입니다.", status_code = 400)
     @status_code = status_code
     @message = message
   end

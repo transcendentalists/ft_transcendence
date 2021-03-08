20c9aba18f3eccff3557e72983f4095b6897f8cc
sanam <simian114@gmail.com>
2021-03-08 09:06:24 +0900

    refactor: 함수 이름 변경
    기존의 valid_of_user 는 동사 형용사의 관계가 이상한 점이 있어서
    valdate_user 동사 + 명사로 수정했습니다.

--- project/app/validators/guild_invitation_validator.rb
+++ project/app/validators/guild_invitation_validator.rb
@@ -9,7 +9,7 @@ class GuildInvitationValidator < ActiveModel::Validator
     @record.errors.add(:base, message: "이미 길드 초대장을 보내셨습니다.") if @invited_user&.already_received_guild_invitation_by(@user)
   end
 
-  def valid_of_user
+  def validate_user
     if @user.nil?
       @record.errors.add(:user_id, message: "잘못된 요청입니다.")
     elsif @user.in_guild.nil?
@@ -19,7 +19,7 @@ class GuildInvitationValidator < ActiveModel::Validator
     end
   end
 
-  def valid_of_invited_user
+  def validate_invited_user
     if @invited_user.nil?
       @record.errors.add(:invited_user_id, message: "해당 유저가 존재하지 않습니다.")
     elsif !@invited_user.in_guild.nil?

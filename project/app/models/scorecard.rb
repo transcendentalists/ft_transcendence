3a65e4e2087ee075768629b2592d64b4b46a535a
sanam <simian114@gmail.com>
2021-03-08 22:21:16 +0900

    feat: opponent_user, opponent_guild 메서드
    war 에는 war_status가 2개 있습니다. 그리고 프론트에서는 war 에 대한
    요청을 하면서 자신의 guild_id 만들던져줍니다. 따라서 war_statuses 를 내
    guild, 상대 guild 두개 가져오기는 조금 번거로운 점이 있었습니다. 따라서
    번거로운 작업없이 바로 가져올 수 있는 메서드를 구현했습니다.

--- project/app/models/scorecard.rb
+++ project/app/models/scorecard.rb
@@ -3,6 +3,14 @@ class Scorecard < ApplicationRecord
   belongs_to :match
   has_one :rule, through: :match
 
+  def opponent_user
+    self.match.users.where.not(id: self.user_id).first
+  end
+
+  def opponent_scorecard
+    self.match.scorecards.where.not(user_id: self.user_id).first
+  end
+
   def ready
     self.update(result: "ready")
   end

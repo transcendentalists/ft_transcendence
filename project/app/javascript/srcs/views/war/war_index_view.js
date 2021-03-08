3d0e04aebd7838e87e7e4f9738fb8cfa73143d35
sanam <simian114@gmail.com>
2021-03-08 22:26:20 +0900

    feat: war_index_view 뼈대 구성
    전체적인 뼈대를 구현했습니다.
    현재 구현 완료된 서브 뷰는
    guild, rules_of_war, match_history 가 있습니다.

--- project/app/javascript/srcs/views/war/war_index_view.js
+++ project/app/javascript/srcs/views/war/war_index_view.js
@@ -1,9 +1,90 @@
-export let WarIndexView = Backbone.View.extend({
-  // template: _.template($("#war-index-view-template").html()),
+import { App, Helper } from "srcs/internal";
 
-  render: function () {},
+export let WarIndexView = Backbone.View.extend({
+  id: "war-index-view",
+  template: _.template($("#war-index-view-template").html()),
+  className: "top-margin",
+
+  initialize: function () {
+    this.guild_profile_card_view = null;
+    this.war_status_view = null;
+    this.war_rule_view = null;
+    this.war_battle_view = null;
+    this.war_match_history_list_view = null;
+  },
+
+  renderGuildProfileCardView: function (guild) {
+    this.guild_profile_card_view = new App.View.GuildProfileCardView({
+      guild: guild
+    });
+    this.guild_profile_card_view
+      .setElement(this.$(".opponent-guild-profile-view"))
+      .render();
+  },
+
+  // Status
+  renderWarStatusView: function (war_status) {
+    this.war_status_view = new App.View.WarStatusView();
+    this.war_status_view
+      .setElement(this.$(".war-status-view"))
+      .render(war_status);
+  },
+
+  // Rule of War
+  renderWarRuleView: function (rule) {
+    this.war_rule_view = new App.View.WarRuleView();
+    this.war_rule_view
+      .setElement(this.$(".war-rule-view"))
+      .render(rule);
+  },
+
+  // Battle
+  // renderWarBattleView: function (battle) {
+  renderWarBattleView: function () {
+    this.war_battle_view = new App.View.WarBattleView();
+    this.war_battle_view
+      .setElement(this.$(".war-battle-view"))
+      .render();
+      // .render(battle);
+  },
+  
+  // 대전기록 
+  renderWarMatchHistory: function (war_matches) {
+    this.war_match_history_list_view = new App.View.WarMatchHistoryListView();
+    this.war_match_history_list_view
+      .setElement(this.$(".war-match-history-list-view"))
+      .render(war_matches);
+  },
+
+  renderWarIndexChilds: function(data) {
+    this.renderGuildProfileCardView(data.guild);
+    this.renderWarStatusView(data.status);
+    this.renderWarRuleView(data.rules_of_war);
+    // this.renderWarBattleView(data.battle);
+    this.renderWarBattleView();
+    this.renderWarMatchHistory(data.matches);
+  },
+
+  render: function () {
+    const current_user_guild = App.current_user.get("guild");
+    const current_user_guild_in_war = current_user_guild?.in_war;
+    this.$el.html(
+      this.template({ current_user_guild: current_user_guild, current_user_guild_in_war: current_user_guild_in_war })
+    );
+    if (current_user_guild && current_user_guild_in_war) {
+      Helper.fetch(`guilds/${current_user_guild.id}/wars?for=index`, {
+        success_callback: this.renderWarIndexChilds.bind(this),
+      });
+    }
+    return this;
+  },
 
   close: function () {
+    if (this.guild_profile_card_view) this.guild_profile_card_view.close();
+    if (this.war_status_view) this.war_status_view.close();
+    if (this.war_rule_view) this.war_rule_view.close();
+    if (this.war_battle_view) this.war_battle_view.close();
+    if (this.war_match_history_list_view) this.war_match_history_list_view.close();
     this.remove();
   },
 });

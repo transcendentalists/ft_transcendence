a466456e177a9d32cafdd448e63d6b4096cb3295
sanam <simian114@gmail.com>
2021-03-08 22:27:22 +0900

    feat: war_status_view
    너무 간단한 뷰라서 애니메이션 효과를 추가했습니다.

--- project/app/javascript/srcs/views/war/war_status_view.js
+++ project/app/javascript/srcs/views/war/war_status_view.js
@@ -4,16 +4,24 @@ export let WarStatusView = Backbone.View.extend({
   initialize: function () {},
 
   render: function (war_status) {
+    war_status.total_point = war_status.my_guild_point + war_status.opponent_guild_point;
+
     this.$el.html(this.template(war_status));
 
-    let dataset = document.getElementById("challenger-point");
-    document.getElementById("challenger-point").dataset.value = war_status.my_guild_point;
-    document.getElementById("enemy-point").dataset.value = war_status.opponent_guild_point;
-    document.getElementById("remained-no-reply-count").dataset.value = war_status.remained_no_reply_count;
+    // let dataset = document.getElementById("my-guild-point");
+    // document.getElementById("my-guild-point").dataset.value = war_status.my_guild_point;
+    // document.getElementById("enemy-point").dataset.value =
+    //   war_status.opponent_guild_point;
 
-    $('#challenger-point').progress({label: 'ratio', text: { ratio: '{value}P' }});
+    // document.getElementById("remained-no-reply-count").dataset.value =
+    //   war_status.remained_no_reply_count;
+    
+    $('#my-guild-point').progress({label: 'ratio', text: { ratio: '{value}P' }});
     $('#enemy-point').progress({label: 'ratio', text: { ratio: '{value}P' }});
-    $('#remained-no-reply-count').progress({label: 'ratio', text: { ratio: '{value}회/{total}회' }});
+    $("#remained-no-reply-count").progress({
+      label: "ratio",
+      text: { ratio: "{value}회/{total}회" },
+    });
 
     return this;
   },

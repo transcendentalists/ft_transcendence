860f3c0add195b58da4026c5b0f29adc12b807b5
Ungchi <dd1360@naver.com>
2021-03-08 13:17:38 +0900

    fix: war_status update 시validate 체크하는 문제 해결
    create 시에lidate 체크하도록 수정

--- project/app/models/war_status.rb
+++ project/app/models/war_status.rb
@@ -1,7 +1,7 @@
 class WarStatus < ApplicationRecord
   belongs_to :guild
   belongs_to :request, class_name: "WarRequest", :foreign_key => "war_request_id"
-  validates_with WarStatusValidator
+  validates_with WarStatusValidator, on: :create
 
   def self.current_guild_war_status(current_guild_id)
     self.find_by_guild_id(current_guild_id)

7de28b5e679ef0bab7ce922471a8f1594d4d93df
humblEgo <injun.woo30000@gmail.com>
2021-03-08 23:14:42 +0900

    feat: guild 관련 active_admin

---
+++ project/app/admin/guilds.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Guild do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :owner_id, :name, :anagram, :image_url, :point
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:owner_id, :name, :anagram, :image_url, :point]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end

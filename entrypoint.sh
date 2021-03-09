0271352b3a1791636e6608256fb54eb082d0b839
iwoo <iwoo@student.42seoul.kr>
2021-03-09 20:30:09 +0900

    fix: add exec command

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ set -e
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
-
-yarn install --check-files && bundle exec rake db:exists && bundle rake db:migrate || bundle exec "$@"
+bundle exec rake db:exists && bundle exec rake db:migrate
+yarn install --check-files && bundle exec "$@"

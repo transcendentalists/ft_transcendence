b46e6f89f63abfd3b456ef9a5bf57fff643a68b0
iwoo <iwoo@student.42seoul.kr>
2021-03-09 22:05:23 +0900

    feat: 첫 실행시 db:seed (테스트 중)

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,10 @@ set -e
 if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
-bundle exec rake db:exists && bundle exec rake db:migrate
+
+(bundle exec rake db:connection_exists && bundle exec rake db:migrate) || (bundle exec rake db:create && bundle rake db:migrate)
+
+# seed 데이터가 셋팅된 적 없다면, seed 데이터 셋팅
+bundle exec rake db:is_first_time && bundle exec rake db:seed
+
 yarn install --check-files && bundle exec "$@"

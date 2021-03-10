344eb561d2f5978142ce936a0a805a2e764e9426
l-yohai <yohan9612@naver.com>
2021-03-10 17:53:50 +0900

    style: []를 .으로 변경

--- project/app/javascript/srcs/views/war/war_match_history_view.js
+++ project/app/javascript/srcs/views/war/war_match_history_view.js
@@ -7,10 +7,10 @@ export let WarMatchHistoryView = Backbone.View.extend({
   initialize: function () {},
 
   render: function (data) {
-    const match_type = data["match"]["match_type"];
-    const user_card = data["current_guild_user_scorecard"];
-    const enemy_user_card = data["opponent_guild_user_scorecard"];
-    const enemy_user = data["opponent_guild_user"];
+    const match_type = data.match.match_type;
+    const user_card = data.current_guild_user_scorecard;
+    const enemy_user_card = data.opponent_guild_user_scorecard;
+    const enemy_user = data.opponent_guild_user;
     this.$el.html(
       this.template({
         match_type,

18457072d02811375beb0f83909f031118d1ab9c
humblEgo <injun.woo30000@gmail.com>
2021-03-11 11:23:07 +0900

    feat: rspec일 때도 retry_set_schedule 방지

--- project/config/initializers/restart_tournament_scheduler.rb
+++ project/config/initializers/restart_tournament_scheduler.rb
@@ -1,5 +1,6 @@
 # 서버 시작시 진행중인 토너먼트들에 대해 스케쥴 재설정
 # rake 작업시엔 실행되지 않도록 조건처리
-unless ( File.basename($0) == 'rake')
+file_base_name = File.basename($0)
+if !( file_base_name == 'rake' && file_base_name == 'rspec')
   Tournament.retry_set_schedule
 end

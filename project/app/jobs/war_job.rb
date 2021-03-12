4ec7da630cf17889027f4f942632c5ec261feddc
Ungchi <dd1360@naver.com>
2021-03-12 17:05:34 +0900

    refactor: war job 리팩토링 진행

--- project/app/jobs/war_job.rb
+++ project/app/jobs/war_job.rb
@@ -3,36 +3,26 @@ class WarJob < ApplicationJob
 
   def perform(war, options)
     @war = war
-    @type = options[:type]
     @options = options
+    type = options[:type]
 
-    case @type
-    when "war_start" then war_start
-    when "war_end" then war_end
+    case type
+    when "war_start" then @war.start
+    when "war_end" then @war.end
     when "war_time_start" then war_time_start
     when "war_time_end" then war_time_end
     when "match_no_reply" then match_no_reply
     end
   end
 
-  def war_start
-    return unless @war.status == "pending"
-    @war.start
-  end
-
-  def war_end
-    return unless @war.status == "progress"
-    @war.end
-  end
-
   def war_time_start
     return unless @war.status == "progress"
-    broadcast_to_war_channel({ type: "war_time_start", current_hour: Time.zone.now.hour })
+    @war.broadcast({ type: "war_time_start", current_hour: Time.zone.now.hour })
   end
 
   def war_time_end
     return unless @war.status == "progress"
-    broadcast_to_war_channel({ type: "war_time_end", current_hour: Time.zone.now.hour })
+    @war.broadcast({ type: "war_time_end", current_hour: Time.zone.now.hour })
   end
 
   def match_no_reply
@@ -44,10 +34,6 @@ class WarJob < ApplicationJob
     no_reply_guild_war_status.save
     @war.end if no_reply_guild_war_status.no_reply_count > @war.request.max_no_reply_count
     match.cancel
-    broadcast_to_war_channel({ type: "no_reply", user_id: waiting_user.id })
-  end
-
-  def broadcast_to_war_channel(msg)
-    ActionCable.server.broadcast("war_channel_#{@war.id.to_s}", msg)
+    @war.broadcast({ type: "no_reply", user_id: waiting_user.id })
   end
 end

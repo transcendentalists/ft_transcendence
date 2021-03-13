964d82e7b04f9be57a2c1339e3950e144748db26
eunhyulkim <valhalla.host@gmail.com>
2021-03-14 03:29:16 +0900

    style: tournament_membership & tournament method 순서 정리

--- project/app/models/tournament_membership.rb
+++ project/app/models/tournament_membership.rb
@@ -2,12 +2,12 @@ class TournamentMembership < ApplicationRecord
   belongs_to :user
   belongs_to :tournament
 
-  def completed?
-    self.status == "completed"
-  end
-
   def next_match
     self.tournament.next_match_of(self.user)
+  end
+
+  def completed?
+    self.status == "completed"
   end
 
   def defeated?

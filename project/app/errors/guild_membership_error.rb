1533fe2ac4a29a6878efe8f103f5b4dc1cf75500
eunhyulkim <valhalla.host@gmail.com>
2021-03-15 11:11:36 +0900

    fix: 사용하지 않는 error 파일 제거

--- project/app/errors/guild_membership_error.rb
+++
@@ -1,7 +0,0 @@
-class GuildMembershipError < StandardError
-  attr_accessor :message, :status_code
-  def initialize(message = "잘못된 요청입니다.", status_code = 400)
-    @status_code = status_code
-    @message = message
-  end
-end 

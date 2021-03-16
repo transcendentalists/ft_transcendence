c1a40f1544df0e6eaf52b25dfc1325a7188d58d8
Ungchi <dd1360@naver.com>
2021-03-16 15:50:04 +0900

    style: war rule 룰 이름 한글로 수정
    Co-authored-by: Yohan Lee <l-yohai@users.noreply.github.com>
    Co-authored-by: eunhyul kim <valhalla.host@gmail.com>
    Co-authored-by: Injun Woo <humblEgo@users.noreply.github.com>
    Co-authored-by: Nam_Ssang <simian114@users.noreply.github.com>

--- project/app/javascript/srcs/views/war/war_rule_view.js
+++ project/app/javascript/srcs/views/war/war_rule_view.js
@@ -1,7 +1,11 @@
+import { Helper } from "srcs/internal";
+
 export let WarRuleView = Backbone.View.extend({
   template: _.template($("#war-rule-view-template").html()),
 
   render: function (war_rule) {
+    war_rule = war_rule;
+    war_rule.rule.name = Helper.translateRule(war_rule.rule.name);
     this.$el.html(this.template(war_rule));
     return this;
   },
